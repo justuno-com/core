f748adc14399b02624907071d5b7167ebfa7f369
Dmitry Fedyuk <dfediuk@gmail.com>
2020-06-14 07:03:49 +0600

    https://github.com/justuno-com/core/issues/21

--- lib/Core/array/main.php
+++ lib/Core/array/main.php
@@ -1,4 +1,18 @@
 <?php
+/**
+ * 2020-06-14 "Port the `df_array` function": https://github.com/justuno-com/core/issues/21
+ * @used-by ju_explode_xpath()
+ * @param mixed|mixed[] $v
+ * @return mixed[]|string[]|float[]|int[]
+ */
+function ju_array($v) {return is_array($v) ? $v : [$v];}
+
+/**
+ * 2020-06-13 "Port the `df_ita` function": https://github.com/justuno-com/core/issues/15
+ * @param \Traversable|array $t
+ * @return array
+ */
+function ju_ita($t) {return is_array($t) ? $t : iterator_to_array($t);}
 
 /**
  * 2020-06-14 "Port the `dfa_flatten` function": https://github.com/justuno-com/core/issues/17
@@ -13,10 +27,3 @@ function jua_flatten(array $a) {
 	array_walk_recursive($a, function($a) use(&$r) {$r[]= $a;});
 	return $r;
 }
-
-/**
- * 2020-06-13 "Port the `df_ita` function": https://github.com/justuno-com/core/issues/15
- * @param \Traversable|array $t
- * @return array
- */
-function ju_ita($t) {return is_array($t) ? $t : iterator_to_array($t);}
--- lib/Core/text/explode.php
+++ lib/Core/text/explode.php
@@ -2,8 +2,7 @@
 /**
  * 2020-06-14 "Port the `df_explode_xpath` function": https://github.com/justuno-com/core/issues/20
  * @used-by jua_deep()
- * @used-by \Df\Config\Backend::value()
  * @param string|string[] $p
  * @return string[]
  */
-function ju_explode_xpath($p) {return jua_flatten(array_map(function($s) {return explode('/', $s);}, df_array($p)));}
+function ju_explode_xpath($p) {return jua_flatten(array_map(function($s) {return explode('/', $s);}, ju_array($p)));}
