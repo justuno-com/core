5db388c338cf8ae61b332a91afea4eefa7c6143a
Dmitry Fedyuk <dfediuk@gmail.com>
2020-06-16 22:56:26 +0600

    https://github.com/justuno-com/core/issues/31

--- lib/Core/array/map.php
+++ lib/Core/array/map.php
@@ -1,11 +1,15 @@
 <?php
 /**
  * 2020-06-16
+ * PHP supports global constants since 5.3:
+ * http://www.codingforums.com/php/303927-unexpected-t_const-php-version-5-2-17-a.html#post1363452
  * @used-by ju_find()
  */
 const DF_AFTER = 1;
 /**
  * 2020-06-16
+ * PHP supports global constants since 5.3:
+ * http://www.codingforums.com/php/303927-unexpected-t_const-php-version-5-2-17-a.html#post1363452
  * @used-by ju_find()
  */
 const DF_BEFORE = -1;
