b91a3286b8f8834503fe9312ba405aa9f41243a4
Dmitry Fedyuk <dfediuk@gmail.com>
2020-06-17 05:58:30 +0600

    https://github.com/justuno-com/core/issues/42

--- lib/Core/text/format.php
+++ lib/Core/text/format.php
@@ -23,5 +23,23 @@ function ju_format(...$args) { /** @var string $r */
 			}
 			break;
 	}
-	return !is_null($r) ? $r : df_sprintf($args);
+	return !is_null($r) ? $r : ju_sprintf($args);
+}
+
+/**
+ * 2020-06-17 "Port the `df_sprintf` function": https://github.com/justuno-com/core/issues/42
+ * @used-by ju_format()
+ * @param string|mixed[] $s
+ * @return string
+ * @throws Exception
+ */
+function ju_sprintf($s) {/** @var string $r */ /** @var mixed[] $args */
+	// 2020-03-02
+	// The square bracket syntax for array destructuring assignment (`[…] = […]`) requires PHP ≥ 7.1:
+	// https://github.com/mage2pro/core/issues/96#issuecomment-593392100
+	// We should support PHP 7.0.
+	list($s, $args) = is_array($s) ? [df_first($s), $s] : [$s, func_get_args()];
+	try {$r = df_sprintf_strict($args);}
+	catch (Exception $e) {$r = $s;}
+	return $r;
 }
