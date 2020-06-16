8d0feba09646c81602f00aac408a6b0c338ac533
Dmitry Fedyuk <dfediuk@gmail.com>
2020-06-16 21:25:49 +0600

    https://github.com/justuno-com/core/issues/28

--- lib/Qa/bt.php
+++ lib/Qa/bt.php
@@ -1,8 +1,21 @@
 <?php
+use Exception as E;
+use Df\Qa\Trace;
+use Df\Qa\Trace\Formatter;
 
 /**
  * 2020-06-16 "Port the `df_bt` function": https://github.com/justuno-com/core/issues/27
  * @used-by \Justuno\Core\Exception::__construct()
  * @param int $levelsToSkip
  */
-function ju_bt($levelsToSkip = 0) {df_report('bt-{date}-{time}.log', df_bt_s(++$levelsToSkip));}
+function ju_bt($levelsToSkip = 0) {df_report('bt-{date}-{time}.log', ju_bt_s(++$levelsToSkip));}
+
+/**
+ * 2020-06-16 "Port the `df_bt_s` function": https://github.com/justuno-com/core/issues/28
+ * @used-by ju_bt()
+ * @param int|E $p
+ * @return string
+ */
+function ju_bt_s($p = 0) {return Formatter::p(
+	new Trace($p instanceof E ? $p->getTrace() : array_slice(debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS), $p))
+);}
