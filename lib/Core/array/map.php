2ccd71290c61122f0f737ec4cc4eb3b4e6a9b0e9
Dmitry Fedyuk <dfediuk@gmail.com>
2020-06-18 04:39:29 +0600

    https://github.com/justuno-com/core/issues/60

--- lib/Core/array/map.php
+++ lib/Core/array/map.php
@@ -6,6 +6,7 @@ use Justuno\Core\Exception as DFE;
  * PHP supports global constants since 5.3:
  * http://www.codingforums.com/php/303927-unexpected-t_const-php-version-5-2-17-a.html#post1363452
  * @used-by ju_find()
+ * @used-by ju_map()
  */
 const JU_AFTER = 1;
 /**
@@ -13,6 +14,7 @@ const JU_AFTER = 1;
  * PHP supports global constants since 5.3:
  * http://www.codingforums.com/php/303927-unexpected-t_const-php-version-5-2-17-a.html#post1363452
  * @used-by ju_find()
+ * @used-by ju_map()
  */
 const JU_BEFORE = -1;
 
@@ -34,20 +36,20 @@ function ju_map($a1, $a2, $pAppend = [], $pPrepend = [], $keyPosition = 0, $retu
 	// The square bracket syntax for array destructuring assignment (`[…] = […]`) requires PHP ≥ 7.1:
 	// https://github.com/mage2pro/core/issues/96#issuecomment-593392100
 	// We should support PHP 7.0.
-	[$a, $f] = dfaf($a1, $a2); /** @var array|\Traversable $a */ /** @var callable $f */
+	[$a, $f] = juaf($a1, $a2); /** @var array|\Traversable $a */ /** @var callable $f */
 	/** @var array(int|string => mixed) $r */
 	if (!$pAppend && !$pPrepend && 0 === $keyPosition && !$returnKey) {
-		$r = array_map($f, df_ita($a));
+		$r = array_map($f, ju_ita($a));
 	}
 	else {
 		$pAppend = ju_array($pAppend); $pPrepend = ju_array($pPrepend);
 		$r = [];
 		foreach ($a as $k => $v) {/** @var int|string $k */ /** @var mixed $v */ /** @var mixed[] $primaryArgument */
 			switch ($keyPosition) {
-				case DF_BEFORE:
+				case JU_BEFORE:
 					$primaryArgument = [$k, $v];
 					break;
-				case DF_AFTER:
+				case JU_AFTER:
 					$primaryArgument = [$v, $k];
 					break;
 				default:
