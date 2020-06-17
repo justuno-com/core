abfa7054e45724eae70a6e01aee1126d7f8a4bdf
Dmitry Fedyuk <dfediuk@gmail.com>
2020-06-18 05:02:30 +0600

    https://github.com/justuno-com/core/issues/63

--- lib/Core/text/concatenate.php
+++ lib/Core/text/concatenate.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * 2020-06-18 "Port the `df_cc_n` function": https://github.com/justuno-com/core/issues/56
+ * 2020-06-18 "Port the `df_cc_n` function": https://github.com/justuno-com/core/issues/63
  * @param string|string[] ...$args
  * @return string
  */
