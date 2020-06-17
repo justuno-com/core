e861999ea9985e4bccef8a4aae8d5b8ee066e3a4
Dmitry Fedyuk <dfediuk@gmail.com>
2020-06-18 04:29:55 +0600

    https://github.com/justuno-com/core/issues/59

--- lib/Core/array/clean.php
+++ lib/Core/array/clean.php
@@ -7,6 +7,28 @@
  * @param mixed ...$k [optional]
  * @return mixed[]
  */
-function ju_clean(array $r, ...$k) {/** @var mixed[] $r */return df_clean_r(
+function ju_clean(array $r, ...$k) {/** @var mixed[] $r */return ju_clean_r(
 	$r, array_merge([false], ju_args($k)), false
 );}
+
+/**
+ * 2020-02-05
+ * 2020-06-18 "Port the `df_clean_r` function": https://github.com/justuno-com/core/issues/59
+ * @see df_clean()
+ * 1) It works recursively.
+ * 2) I does not remove `false`.
+ * @used-by ju_clean()
+ * @used-by ju_clean_r()
+ * @param mixed[] $r
+ * @param mixed[] $k
+ * @param bool $req [optional]
+ * @return mixed[]
+ */
+function ju_clean_r(array $r, $k = [], $req = true) {/** @var mixed[] $r */
+	/** 2020-02-05 @see array_unique() does not work correctly here, even with the @see SORT_REGULAR flag. */
+	$k = array_merge($k, ['', null, []]);
+	if ($req) {
+		$r = df_map($r, function($v) use($k) {return !is_array($v) ? $v : ju_clean_r($v, $k);});
+	}
+	return df_filter($r, function($v) use($k) {return !in_array($v, $k, true);});
+}
