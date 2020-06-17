ed6e8f5784c6e0848ea18e71ee863e2d48cea49a
Dmitry Fedyuk <dfediuk@gmail.com>
2020-06-17 06:59:47 +0600

    https://github.com/justuno-com/core/issues/49

--- lib/Core/cli.php
+++ lib/Core/cli.php
@@ -1,11 +1,20 @@
 <?php
 /**
+ * 2017-03-15
+ * 2020-06-17 "Port the `df_cli_argv` function": https://github.com/justuno-com/core/issues/49
+ * @used-by ju_cli_script()
+ * @param int $i [optional]
+ * @return string|string[]
+ */
+function ju_cli_argv($i = null) {return jua(jua($_SERVER, 'argv', []), $i);}
+
+/**
  * 2020-02-15
  * 2020-06-17 "Port the `df_cli_script` function": https://github.com/justuno-com/core/issues/48
  * @used-by ju_is_bin_magento()
  * @return string
  */
-function ju_cli_script() {return df_cli_argv(0);}
+function ju_cli_script() {return ju_cli_argv(0);}
 
 /**
  * 2020-02-15
