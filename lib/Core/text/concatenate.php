a5be78178afb01118b42cf44ad9e07b6e105c93a
Dmitry Fedyuk <dfediuk@gmail.com>
2020-06-18 03:36:23 +0600

    https://github.com/justuno-com/core/issues/57

--- lib/Core/text/concatenate.php
+++ lib/Core/text/concatenate.php
@@ -4,4 +4,13 @@
  * @param string|string[] ...$args
  * @return string
  */
-function ju_cc_n(...$args) {return df_ccc("\n", dfa_flatten($args));}
+function ju_cc_n(...$args) {return ju_ccc("\n", dfa_flatten($args));}
+
+/**
+ * 2020-06-18 "Port the `df_ccc` function": https://github.com/justuno-com/core/issues/57
+ * @used-by ju_cc_n
+ * @param string $glue
+ * @param string|string[] ...$elements
+ * @return string
+ */
+function ju_ccc($glue, ...$elements) {return implode($glue, df_clean(dfa_flatten($elements)));}
