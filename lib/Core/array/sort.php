a96ab6a38c32276ab4b80246771b02252fe982a1
Dmitry Fedyuk <dfediuk@gmail.com>
2020-06-18 05:59:18 +0600

    https://github.com/justuno-com/core/issues/68

--- lib/Core/array/sort.php
+++ lib/Core/array/sort.php
@@ -1,5 +1,18 @@
 <?php
 /**
+ * 2017-07-05
+ * 2020-06-18 "Port the `df_ksort_r` function": https://github.com/justuno-com/core/issues/68
+ * @used-by ju_ksort_r()
+ * @used-by ju_ksort_r_ci()
+ * @param array(int|string => mixed) $a
+ * @param callable|null $f [optional]
+ * @return array(int|string => mixed)
+ */
+function ju_ksort_r(array $a, $f = null) {return df_ksort(df_map_k(function($k, $v) use($f) {return
+	!is_array($v) ? $v : ju_ksort_r($v, $f)
+;}, $a), $f);}
+
+/**
  * 2017-08-22
  * 2017-09-07 Be careful! If the $a array is not associative,
  * then df_ksort_r($a, 'strcasecmp') will convert the numeric arrays to associative ones,
@@ -9,4 +22,4 @@
  * @param array(int|string => mixed) $a       
  * @return array(int|string => mixed)
  */
-function ju_ksort_r_ci(array $a) {return !ju_is_assoc($a) ? $a : df_ksort_r($a, 'strcasecmp');}
+function ju_ksort_r_ci(array $a) {return !ju_is_assoc($a) ? $a : ju_ksort_r($a, 'strcasecmp');}
