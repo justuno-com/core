336d070c52b2731be49401514adb27593fe32c1d
Dmitry Fedyuk <dfediuk@gmail.com>
2020-06-18 05:55:44 +0600

    https://github.com/justuno-com/core/issues/67

--- lib/Core/array/sort.php
+++ lib/Core/array/sort.php
@@ -5,8 +5,8 @@
  * then df_ksort_r($a, 'strcasecmp') will convert the numeric arrays to associative ones,
  * and their numeric keys will be ordered as strings.
  * 2020-06-18 "Port the `df_ksort_r_ci` function": https://github.com/justuno-com/core/issues/67
- * @used-by df_json_sort()
- * @param array(int|string => mixed) $a
+ * @used-by ju_json_sort()
+ * @param array(int|string => mixed) $a       
  * @return array(int|string => mixed)
  */
 function ju_ksort_r_ci(array $a) {return !ju_is_assoc($a) ? $a : df_ksort_r($a, 'strcasecmp');}
