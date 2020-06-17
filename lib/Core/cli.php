2dac4d34da84e93b60f87a8559c2ef86875253b3
Dmitry Fedyuk <dfediuk@gmail.com>
2020-06-17 06:56:39 +0600

    https://github.com/justuno-com/core/issues/48

--- lib/Core/cli.php
+++ lib/Core/cli.php
@@ -1,13 +1,21 @@
 <?php
 /**
  * 2020-02-15
+ * 2020-06-17 "Port the `df_cli_script` function": https://github.com/justuno-com/core/issues/48
+ * @used-by ju_is_bin_magento()
+ * @return string
+ */
+function ju_cli_script() {return df_cli_argv(0);}
+
+/**
+ * 2020-02-15
  * 1) `bin/magento` can be called with a path prefix, so I use @uses ju_ends_with()
  * 2) df_cli_script() returns «bin/magento» even in the `php bin/magento ...` case.
  * 2020-06-17 "Port the `df_is_bin_magento` function": https://github.com/justuno-com/core/issues/46
  * @used-by ju_is_cron()
  * @return bool
  */
-function ju_is_bin_magento() {return ju_ends_with(df_cli_script(), 'bin/magento');}
+function ju_is_bin_magento() {return ju_ends_with(ju_cli_script(), 'bin/magento');}
 
 /**
  * 2016-10-25 http://stackoverflow.com/a/1042533
