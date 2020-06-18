f1d32c38c90ad6d5a4be96d88c57c1ded81692a0
Dmitry Fedyuk <dfediuk@gmail.com>
2020-06-18 06:18:53 +0600

    https://github.com/justuno-com/core/issues/69

--- lib/Core/array/sort.php
+++ lib/Core/array/sort.php
@@ -1,5 +1,15 @@
 <?php
 /**
+ * 2016-01-29
+ * 2020-06-18 "Port the `df_ksort` function": https://github.com/justuno-com/core/issues/69
+ * @used-by ju_ksort_r()
+ * @param array(int|string => mixed) $a
+ * @param callable|null $f [optional]
+ * @return array(int|string => mixed)
+ */
+function ju_ksort(array $a, $f = null) {$f ? uksort($a, $f) : ksort($a); return $a;}
+
+/**
  * 2017-07-05
  * 2020-06-18 "Port the `df_ksort_r` function": https://github.com/justuno-com/core/issues/68
  * @used-by ju_ksort_r()
@@ -8,7 +18,7 @@
  * @param callable|null $f [optional]
  * @return array(int|string => mixed)
  */
-function ju_ksort_r(array $a, $f = null) {return df_ksort(df_map_k(function($k, $v) use($f) {return
+function ju_ksort_r(array $a, $f = null) {return ju_ksort(df_map_k(function($k, $v) use($f) {return
 	!is_array($v) ? $v : ju_ksort_r($v, $f)
 ;}, $a), $f);}
 
