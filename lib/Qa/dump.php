9d6fc3d269b13421359132f9199a9465502ba2e4
Dmitry Fedyuk <dfediuk@gmail.com>
2020-06-18 08:24:18 +0600

    https://github.com/justuno-com/core/issues/81

--- lib/Qa/dump.php
+++ lib/Qa/dump.php
@@ -1,4 +1,14 @@
 <?php
+use Df\Qa\Dumper;
+
+/**
+ * 2020-06-18 "Port the `df_dump` function": https://github.com/justuno-com/core/issues/81
+ * @used-by ju_type()
+ * @param \Magento\Framework\DataObject|mixed[]|mixed $v
+ * @return string
+ */
+function ju_dump($v) {return Dumper::i()->dump($v);}
+
 /**
  * 2015-04-05
  * 2020-06-18 "Port the `df_type` function": https://github.com/justuno-com/core/issues/80
@@ -6,8 +16,8 @@
  * @param mixed $v
  * @return string
  */
-function ju_type($v) {return is_object($v) ? sprintf('an object: %s', get_class($v), df_dump($v)) : (is_array($v)
-	? (10 < ($c = count($v)) ? "«an array of $c elements»" : 'an array: ' . df_dump($v))
+function ju_type($v) {return is_object($v) ? sprintf('an object: %s', get_class($v), ju_dump($v)) : (is_array($v)
+	? (10 < ($c = count($v)) ? "«an array of $c elements»" : 'an array: ' . ju_dump($v))
 	/** 2020-02-04 We should not use @see df_desc() here */
 	: (is_null($v) ? '`null`' : sprintf('«%s» (%s)', df_string($v), gettype($v)))
 );}
