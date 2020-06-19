01736b732cf13f4533c30b6c5fb6db0abbd44139
Dmitry Fedyuk <dfediuk@gmail.com>
2020-06-20 05:20:37 +0600

    https://github.com/justuno-com/core/issues/86

--- lib/Core/text/explode.php
+++ lib/Core/text/explode.php
@@ -1,5 +1,14 @@
 <?php
 /**
+ * 2018-04-24 I have added @uses trim() today.
+ * 2020-06-20 "Port the `df_explode_n` function": https://github.com/justuno-com/core/issues/86
+ * @used-by ju_tab_multiline()
+ * @param string $s
+ * @return string[]
+ */
+function ju_explode_n($s) {return explode("\n", df_normalize(df_trim($s)));}
+
+/**
  * 2020-06-14 "Port the `df_explode_xpath` function": https://github.com/justuno-com/core/issues/20
  * @used-by jua_deep()
  * @param string|string[] $p
