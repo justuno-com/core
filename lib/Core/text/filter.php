ed05618570c4e5dd8e6b2ec8b1787cb4552c0884
Dmitry Fedyuk <dfediuk@gmail.com>
2020-06-20 05:34:02 +0600

    https://github.com/justuno-com/core/issues/87

--- lib/Core/text/filter.php
+++ lib/Core/text/filter.php
@@ -2,6 +2,7 @@
 /**
  * 2020-06-20 "Port the `df_normalize` function": https://github.com/justuno-com/core/issues/87
  * http://darklaunch.com/2009/05/06/php-normalize-newlines-line-endings-crlf-cr-lf-unix-windows-mac
+ * @used-by ju_explode_n()
  * @param string $s
  * @return string
  */
