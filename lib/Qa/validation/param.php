2d3c76d2398b0f14aba8b9a7dfc33bd73425a2ec
Dmitry Fedyuk <dfediuk@gmail.com>
2020-06-21 07:44:25 +0600

    https://github.com/justuno-com/core/issues/22

--- lib/Qa/validation/param.php
+++ lib/Qa/validation/param.php
@@ -15,3 +15,17 @@ use Justuno\Core\Exception as DFE;
 function ju_param_s($v, $ord, $sl = 0) {$sl++;
 	return Q::assertValueIsString($v, $sl) ? $v : Q::raiseErrorParam(__FUNCTION__, $ms = [Q::S], $ord, $sl);
 }
+
+/**
+ * 2020-06-15 "Port the `df_param_sne` function": https://github.com/justuno-com/core/issues/22
+ * @used-by jua_deep()
+ * @param string $v
+ * @param int $ord	zero-based
+ * @param int $sl [optional]
+ * @return string
+ * @throws DFE
+ */
+function ju_param_sne($v, $ord, $sl = 0) {$sl++;
+	Q::assertValueIsString($v, $sl);
+	return '' !== strval($v) ? $v : Q::raiseErrorParam(__FUNCTION__, $ms = [Q::NES], $ord, $sl);
+}
