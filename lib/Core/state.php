ff095208137eea0d724c558e541b98da681e6ddc
Dmitry Fedyuk <dfediuk@gmail.com>
2020-06-24 11:46:54 +0600

    https://github.com/justuno-com/core/issues/129

--- lib/Core/state.php
+++ lib/Core/state.php
@@ -9,3 +9,12 @@ use Magento\Framework\App\State;
  * @return State
  */
 function ju_app_state() {return ju_o(State::class);}
+
+/**
+ * https://mage2.ru/t/94
+ * https://mage2.pro/t/59
+ * 2020-06-24 "Port the `df_is_ajax` function": https://github.com/justuno-com/core/issues/129
+ * @used-by ju_is_backend()
+ * @return bool
+ */
+function ju_is_ajax() {static $r; return !is_null($r) ? $r : $r = ju_request_o()->isXmlHttpRequest();}
