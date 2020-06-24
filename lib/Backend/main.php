0f2f9f5776618b4fca13b18328d68479665d51d4
Dmitry Fedyuk <dfediuk@gmail.com>
2020-06-24 12:12:22 +0600

    https://github.com/justuno-com/core/issues/131

--- lib/Backend/main.php
+++ lib/Backend/main.php
@@ -7,8 +7,16 @@ use Magento\User\Model\User;
 
 /**
  * 2016-12-23
+ * 2020-06-24 "Port the `df_backend_session` function": https://github.com/justuno-com/core/issues/131
+ * @used-by ju_backend_user()
+ * @return SessionB
+ */
+function ju_backend_session() {return ju_o(SessionB::class);}
+
+/**
+ * 2016-12-23
  * 2020-06-24 "Port the `df_backend_user` function": https://github.com/justuno-com/core/issues/130
  * @used-by ju_is_backend()
  * @return User|null
  */
-function ju_backend_user() {return df_backend_session()->getUser();}
+function ju_backend_user() {return ju_backend_session()->getUser();}
