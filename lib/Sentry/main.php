af9d26a5d21441df9884f96ed85498faec589670
Dmitry Fedyuk <dfediuk@gmail.com>
2020-06-24 08:57:24 +0600

    https://github.com/justuno-com/core/issues/118

---
+++ lib/Sentry/main.php
@@ -0,0 +1,46 @@
+<?php
+use Df\Core\Exception as DFE;
+use Df\Sentry\Client as Sentry;
+use Exception as E;
+use Magento\Framework\DataObject as _DO;
+use Magento\User\Model\User;
+/**
+ * 2016-12-22
+ * $m could be:
+ * 1) A module name: «A_B»
+ * 2) A class name: «A\B\C».
+ * 3) An object: it comes down to the case 2 via @see get_class()
+ * 4) `null`: it comes down to the case 1 with the «Df_Core» module name.
+ * 2020-06-24 "Port the `df_sentry` function": https://github.com/justuno-com/core/issues/118
+ * @used-by ju_log()
+ * @param string|object|null $m
+ * @param _DO|mixed[]|mixed|E $v
+ * @param array(string => mixed) $context [optional]
+ */
+function ju_sentry($m, $v, array $context = []) {
+	static $domainsToSkip = []; /** @var string[] $domainsToSkip */
+	if ($v instanceof E || !in_array(df_domain_current(), $domainsToSkip)) {
+		$m = df_sentry_module($m);
+		static $d; /** @var array(string => mixed) $d */
+		$d = $d ?: ['extra' => [], 'fingerprint' => [
+			df_core_version(), df_domain_current(), df_magento_version(), df_package_version($m), df_store_code()
+		]];
+		// 2017-01-09
+		if ($v instanceof DFE) {
+			$context = df_extend($context, $v->sentryContext());
+		}
+		$context = df_extend($d, $context);
+		if ($v instanceof E) {
+			// 2016-12-22 https://docs.sentry.io/clients/php/usage/#reporting-exceptions
+			df_sentry_m($m)->captureException($v, $context);
+		}
+		else {
+			$v = df_dump($v);
+			// 2016-12-22 https://docs.sentry.io/clients/php/usage/#reporting-other-errors
+			df_sentry_m($m)->captureMessage($v, [], [
+				'fingerprint' => array_merge(dfa($context, 'fingerprint', []), [$v])
+				,'level' => Sentry::DEBUG
+			] + $context);
+		}
+	}
+}
