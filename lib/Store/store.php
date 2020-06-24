046977165310355fa5a94aae7595430485c9fba2
Dmitry Fedyuk <dfediuk@gmail.com>
2020-06-24 10:06:13 +0600

    https://github.com/justuno-com/core/issues/121

--- lib/Store/store.php
+++ lib/Store/store.php
@@ -13,9 +13,20 @@ use Magento\Store\Model\StoreResolver;
 
 /**
  * 2017-03-15 Returns an empty string if the store's root URL is absent in the Magento database.
+ * 2020-06-24 "Port the `df_store_url` function": https://github.com/justuno-com/core/issues/121
+ * @used-by df_store_url_link()
+ * @used-by df_store_url_web()
+ * @param int|string|null|bool|IStore $s
+ * @param string $type
+ * @return string
+ */
+function ju_store_url($s, $type) {return df_store($s)->getBaseUrl($type);}
+
+/**
+ * 2017-03-15 Returns an empty string if the store's root URL is absent in the Magento database.
  * 2020-06-24 "Port the `df_store_url_web` function": https://github.com/justuno-com/core/issues/120
  * @used-by df_domain_current()
  * @param int|string|null|bool|IStore $s [optional]
  * @return string
  */
-function ju_store_url_web($s = null) {return df_store_url($s, U::URL_TYPE_WEB);}
+function ju_store_url_web($s = null) {return ju_store_url($s, U::URL_TYPE_WEB);}
