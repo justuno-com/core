4e2a170f39848e8377305e076311787618b33d76
Dmitry Fedyuk <dfediuk@gmail.com>
2020-06-24 11:42:09 +0600

    https://github.com/justuno-com/core/issues/127

--- lib/Core/area.php
+++ lib/Core/area.php
@@ -2,6 +2,24 @@
 use Magento\Framework\App\Area as A;
 
 /**
+ * 2017-04-02 «Area code is not set» on a df_area_code_is() call: https://mage2.pro/t/3581
+ * 2020-06-24 "Port the `df_area_code` function": https://github.com/justuno-com/core/issues/127
+ * @used-by ju_area_code_is()
+ * @param bool $throw [optional]
+ * @return string|null
+ * @throws \Exception
+ */
+function ju_area_code($throw = true) {
+	try {return df_app_state()->getAreaCode();}
+	catch (\Exception $e) {
+		if ($throw) {
+			throw $e;
+		}
+		return null;
+	}
+}
+
+/**
  * 2016-09-30
  * 2017-04-02 «Area code is not set» on a df_area_code_is() call: https://mage2.pro/t/3581
  * 2020-06-24 "Port the `df_area_code_is` function": https://github.com/justuno-com/core/issues/126
@@ -9,7 +27,7 @@ use Magento\Framework\App\Area as A;
  * @param string ...$values
  * @return bool
  */
-function ju_area_code_is(...$values) {return ($a = df_area_code(false)) && in_array($a, $values);}
+function ju_area_code_is(...$values) {return ($a = ju_area_code(false)) && in_array($a, $values);}
 
 /**
  * 2015-08-14
