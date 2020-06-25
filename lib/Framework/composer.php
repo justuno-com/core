ed4d04cd5ae74377380471937cb114f6c72f466d
Dmitry Fedyuk <dfediuk@gmail.com>
2020-06-26 03:11:15 +0600

    https://github.com/justuno-com/core/issues/144

--- lib/Framework/composer.php
+++ lib/Framework/composer.php
@@ -7,4 +7,18 @@ use Magento\Framework\Config\Composer\Package;
  * @used-by ju_sentry()
  * @return string
  */
-function ju_core_version() {return jucf(function() {return df_package_version('Justuno_Core');});}
+function ju_core_version() {return jucf(function() {return ju_package_version('Justuno_Core');});}
+
+/**
+ * 2016-06-26
+ * The method can be used not only for custom packages, but for standard Magento packages too.
+ * «How to programmatically get an extension's version from its composer.json file?» https://mage2.pro/t/1798
+ * 2017-04-10
+ * From now on, the function gets the package's name from the package's `composer.json` file only.
+ * A package's name as $m is not allowed anymore.
+ * 2020-06-26 "Port the `df_package_version` function": https://github.com/justuno-com/core/issues/144
+ * @used-by ju_core_version()
+ * @param string|object|null $m [optional]
+ * @return string|null
+ */
+function ju_package_version($m = null) {return df_package($m, 'version');}
