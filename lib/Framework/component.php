77fe93f5b44b6f775a24015ab57c2e67a09f607c
Dmitry Fedyuk <dfediuk@gmail.com>
2020-06-26 10:32:41 +0600

    https://github.com/justuno-com/core/issues/150

--- lib/Framework/component.php
+++ lib/Framework/component.php
@@ -10,4 +10,16 @@ use Magento\Framework\Component\ComponentRegistrar as R;
  * @used-by ju_module_dir()
  * @return string
  */
-function ju_framework_path() {return df_lib_path('magento/framework');}
+function ju_framework_path() {return ju_lib_path('magento/framework');}
+
+/**
+ * 2019-12-31
+ * It returns the fill filesystem path of a library, e.g.:
+ * «C:/work/clients/royalwholesalecandy.com-2019-12-08/code/vendor/magento/framework»
+ * or «C:/work/clients/royalwholesalecandy.com-2019-12-08/code/lib/internal/magento/framework»
+ * 2020-06-26 "Port the `df_lib_path` function": https://github.com/justuno-com/core/issues/150
+ * @used-by ju_framework_path()
+ * @param string $l
+ * @return string
+ */
+function ju_lib_path($l) {return df_component_r()->getPath(R::LIBRARY, $l);}
