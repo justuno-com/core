4a44cadb016f9980d129cea12531afd837f8e96a
Dmitry Fedyuk <dfediuk@gmail.com>
2020-08-13 16:31:24 +0600

    https://github.com/justuno-com/core/issues/161

--- lib/Sentry/main.php
+++ lib/Sentry/main.php
@@ -48,53 +48,32 @@ function ju_sentry($m, $v, array $context = []) {
 /**
  * 2016-12-22
  * $m could be:
- * 1) A module name: «A_B»
- * 2) A class name: «A\B\C».
- * 3) An object: it comes down to the case 2 via @see get_class()
+ * 1) a module name: «A_B»
+ * 2) a class name: «A\B\C».
+ * 3) an object: it comes down to the case 2 via @see get_class()
  * 4) `null`: it comes down to the case 1 with the «Justuno_Core» module name.
  * 2020-06-26 "Port the `df_sentry_m` function": https://github.com/justuno-com/core/issues/161
  * @used-by ju_sentry()
- * @used-by ju_sentry_m()
+ * @used-by ju_sentry_m())
  * @param string|object|null $m
  * @return Sentry
  */
 function ju_sentry_m($m) {return dfcf(function($m) {
 	$r = null; /** @var Sentry $r */
-	/** @var array(string => mixed) $a */ /** @var array(string => string)|null $sa */
+	/** @var array(string => $r) $a */ /** @var array(string => string)|null $sa */
 	if (($a = ju_module_json($m, 'df', false)) && ($sa = jua($a, 'sentry'))) {
-		// 2018-08-25
-		$domain = ($id = intval($sa['id'])) < 1000 ? 'log.mage2.pro' : 'sentry.io'; /** @var int $id */
-		$r = new Sentry("https://{$sa['key1']}:{$sa['key2']}@$domain/{$sa['id']}", [
-			/**
-			 * 2016-12-22
-			 * i do not use @see \Justuno\Core\Sentry\Client::getDefaultPrefixes()
-			 * because it includes all @see get_include_path()
-			 * including system folders inside the Magento root folder (e.g. `lib\internal`),
-			 * and a path like `C:\work\mage2.pro\store\lib\internal\Magento\Framework\App\ErrorHandler.php`
-			 * will be chopped as `Magento\Framework\App\ErrorHandler.php`
-			 */
-			'prefixes' => [BP . DIRECTORY_SEPARATOR]
-			,'processors' => [] /** 2016-12-25 To skip @see \Justuno\Core\Sentry\SanitizeDataProcessor */
-		]);
-		/**
-		 * 2016-12-22
-		 * «The root path to your application code.»
-		 * https://docs.sentry.io/clients/php/config/#available-settings
-		 * Airbrake for Ruby provides a similar option: «root_directory»:
-		 * https://github.com/airbrake/airbrake-ruby/blob/v1.6.0/README.md#root_directory
-		 */
-		$r->setAppPath(BP);
+		$r = new Sentry(intval($sa['id']), $sa['key1'], $sa['key2']);
 		// 2016-12-23 https://docs.sentry.io/clientdev/interfaces/user
 		/** @var User|null $u */
-		$r->user_context((df_is_cli() ? ['username' => df_cli_user()] : (
+		$r->user((ju_is_cli() ? ['username' => df_cli_user()] : (
 			($u = ju_backend_user()) ? [
 				'email' => $u->getEmail(), 'id' => $u->getId(), 'username' => $u->getUserName()
 			] : (!df_is_frontend() ? [] : (($c = df_customer())
 				? ['email' => $c->getEmail(), 'id' => $c->getId(), 'username' => $c->getName()]
 				: ['id' => df_customer_session_id()]
 			))
-		)) + ['ip_address' => df_visitor_ip()], false);
-		$r->tags_context([
+			)) + ['ip_address' => df_visitor_ip()], false);
+		$r->tags([
 			'Core' => ju_core_version()
 			,'Magento' => ju_magento_version()
 			,'MySQL' => df_db_version()
