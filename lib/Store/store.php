75469b82d0e1c5655e63ccd3579b679a6c7d3e53
Dmitry Fedyuk <dfediuk@gmail.com>
2020-08-21 11:27:27 +0600

    no message

--- lib/Store/store.php
+++ lib/Store/store.php
@@ -1,11 +1,9 @@
 <?php
-use Df\Directory\Model\Country;
 use Magento\Framework\App\ScopeInterface as IScope;
 use Magento\Framework\Exception\NoSuchEntityException as NSE;
 use Magento\Framework\UrlInterface as U;
 use Magento\Sales\Model\Order as O;
 use Magento\Store\Api\Data\StoreInterface as IStore;
-use Magento\Store\Model\Information as Inf;
 use Magento\Store\Model\Store;
 use Magento\Store\Model\StoreManager;
 use Magento\Store\Model\StoreManagerInterface as IStoreManager;
