c16817cc40246d5eb4a5c280c560836110ce2fae
Dmitry Fedyuk <dfediuk@gmail.com>
2020-08-21 06:04:35 +0600

    https://github.com/justuno-com/core/issues/213

--- lib/Framework/url/o.php
+++ lib/Framework/url/o.php
@@ -1,12 +1,11 @@
 <?php
-use Df\Backend\Model\Url as UrlBackend;
+use Magento\Backend\Model\Url as UrlBackendM;
 use Magento\Backend\Model\UrlInterface as IUrlBackend;
 use Magento\Framework\Url;
 use Magento\Framework\UrlInterface as IUrl;
-
 /**
  * 2020-08-21 "Port the `ju_url_o` function" https://github.com/justuno-com/core/issues/213
  * @used-by ju_current_url()
- * @return IUrl|Url|IUrlBackend|UrlBackend
+ * @return IUrl|Url|IUrlBackend|UrlBackendM
  */
 function ju_url_o() {return ju_o(IUrl::class);}
