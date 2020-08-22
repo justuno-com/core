a48e83b5f59a30fd727ff287295ce336413b4502
Dmitry Fedyuk <dfediuk@gmail.com>
2020-08-22 12:55:10 +0600

    ttps://github.com/justuno-com/core/issues/257

--- Config/Source.php
+++ Config/Source.php
@@ -23,18 +23,6 @@ abstract class Source extends SourceBase {
 	abstract protected function map();
 
 	/**
-	 * 2017-03-28
-	 * @used-by \Magento\Config\Model\Config\Structure\Element\Field::_getOptionsFromSourceModel()
-	 *		$sourceModel = $this->_sourceFactory->create($sourceModel);
-	 *		if ($sourceModel instanceof \Magento\Framework\DataObject) {
-	 *			$sourceModel->setPath($this->getPath());
-	 *		}
-	 * https://github.com/magento/magento2/blob/2.1.5/app/code/Magento/Config/Model/Config/Structure/Element/Field.php#L435-L438
-	 * @param string $v
-	 */
-	final function setPath($v) {$this->_path = $v;}
-
-	/**
 	 * 2015-11-27
 	 * @override
 	 * @see \Magento\Framework\Option\ArrayInterface::toOptionArray()                             
@@ -43,12 +31,4 @@ abstract class Source extends SourceBase {
 	 * @return array(array('label' => string, 'value' => int|string))
 	 */
 	final function toOptionArray() {return ju_map_to_options_t($this->map());}
-
-	/**
-	 * 2017-03-28
-	 * @used-by pathA()
-	 * @used-by setPath()
-	 * @var string
-	 */
-	private $_path;
 }
