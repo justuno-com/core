8e15ff60e89e3dbdb00647c019c5cd2e89b51e51
Dmitry Fedyuk <dfediuk@gmail.com>
2020-08-23 09:59:26 +0600

    https://github.com/justuno-com/core/issues/294

--- lib/Core/lang/call.php
+++ lib/Core/lang/call.php
@@ -103,12 +103,11 @@ function ju_call_a(callable $f, array $parentArgs, $pAppend = [], $pPrepend = []
  *	 a(function() {return 0;}); возвращает true
  * https://3v4l.org/MfmCj
  *	is_callable('intval') возвращает true
+ * @used-by ju_if()
  * @used-by ju_if1()
  * @used-by jua()
  * @param mixed|callable $v
  * @param mixed ...$a [optional]
  * @return mixed
  */
-function ju_call_if($v, ...$a) {return
-	is_callable($v) && !is_string($v) && !is_array($v) ? call_user_func_array($v, $a) : $v
-;}
+function ju_call_if($v, ...$a) {return is_callable($v) && !is_string($v) && !is_array($v) ? call_user_func_array($v, $a) : $v;}
--- lib/Core/lang/main.php
+++ lib/Core/lang/main.php
@@ -13,13 +13,14 @@ use Magento\Framework\App\Filesystem\DirectoryList;
 function ju_args(array $a) {return !$a || !is_array($a[0]) ? $a : $a[0];}
 
 /**
+ * 2020-08-23 "Port the `df_if` function" https://github.com/justuno-com/core/issues/294
  * @used-by ju_cfg()
  * @param bool $cond
  * @param mixed|callable $onTrue
  * @param mixed|null|callable $onFalse [optional]
  * @return mixed
  */
-function ju_if($cond, $onTrue, $onFalse = null) {return $cond ? df_call_if($onTrue) : df_call_if($onFalse);}
+function ju_if($cond, $onTrue, $onFalse = null) {return $cond ? ju_call_if($onTrue) : ju_call_if($onFalse);}
 
 /**
  * 2020-06-13 "Port the `df_if1` function": https://github.com/justuno-com/core/issues/10
