9148aa33ea9c96d314de4b1d98940a50b50f8957
Dmitry Fedyuk <dfediuk@gmail.com>
2020-08-23 10:14:17 +0600

    https://github.com/justuno-com/core/issues/3

--- lib/Core/handle.php
+++ lib/Core/handle.php
@@ -6,4 +6,12 @@
  * @param string $n
  * @return bool
  */
-function ju_handle($n) {return in_array($n, df_handles());}
+function ju_handle($n) {return in_array($n, ju_handles());}
+
+/**
+ * 2015-12-21
+ * 2020-08-23 "Port the `df_handles` function" https://github.com/justuno-com/core/issues/300
+ * @used-by ju_handle()
+ * @return string[]
+ */
+function ju_handles() {return ($u = df_layout_update(null)) ? $u->getHandles() : [];}
