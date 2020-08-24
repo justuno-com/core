f9fce21c7888de0c81ae8b9404b168525a116cea
Dmitry Fedyuk <dfediuk@gmail.com>
2020-08-24 23:20:05 +0600

    https://github.com/justuno-com/core/issues/305

--- lib/Catalog/product/main.php
+++ lib/Catalog/product/main.php
@@ -1,5 +1,12 @@
 <?php
 use Magento\Catalog\Model\Product as P;
+/**
+ * 2019-11-15
+ * 2020-08-24 "Port the `df_product_current_id` function" https://github.com/justuno-com/core/issues/305
+ * @used-by \Justuno\M2\Block\Js::_toHtml()
+ * @return int|null
+ */
+function ju_product_current_id() {return !($p = df_product_current() /** @var P $p */) ? null : $p->getId();}
 
 /**
  * 2019-11-18
