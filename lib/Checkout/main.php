287c7da6d75fb1dca15752e58287dbd2478d1f89
Dmitry Fedyuk <dfediuk@gmail.com>
2020-08-25 11:45:07 +0600

    https://github.com/justuno-com/core/issues/313

--- lib/Checkout/main.php
+++ lib/Checkout/main.php
@@ -1,4 +1,5 @@
 <?php
+use Justuno\Core\Checkout\Model\Session as DfSession;
 use Justuno\Core\Exception as DFE;
 use Magento\Checkout\Model\Session;
 use Magento\Sales\Model\Order as O;
@@ -6,7 +7,7 @@ use Magento\Sales\Model\Order as O;
  * 2016-05-06
  * 2020-08-24 "Port the `df_checkout_session` function" https://github.com/justuno-com/core/issues/312
  * @used-by ju_order_last()
- * @return Session
+ * @return Session|DfSession
  */
 function ju_checkout_session() {return ju_o(Session::class);}
 
@@ -19,6 +20,6 @@ function ju_checkout_session() {return ju_o(Session::class);}
  * @throws DFE
  */
 function ju_order_last($required = true) {
-	$s = ju_checkout_session(); /** @var Session $s */
+	$s = ju_checkout_session(); /** @var Session|DfSession $s */
 	return $s->getLastRealOrderId() ? $s->getLastRealOrder() : (!$required ? null : ju_error());
 }
