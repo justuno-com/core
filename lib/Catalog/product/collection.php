4289259bac5ddec31404071473dc2cc7c566a17e
Dmitry Fedyuk <dfediuk@gmail.com>
2020-11-24 03:40:12 +0600

    1.0.3: https://github.com/mage2pro/core/issues/148

--- lib/Catalog/product/collection.php
+++ lib/Catalog/product/collection.php
@@ -1,4 +1,6 @@
 <?php
+use Closure as F;
+use Justuno\Core\InventoryCatalog\Plugin\Model\ResourceModel\AddStockDataToCollection as PAddStock;
 use Magento\Catalog\Model\ResourceModel\Product\Collection as C;
 /**
  * 2019-09-18
@@ -8,3 +10,37 @@ use Magento\Catalog\Model\ResourceModel\Product\Collection as C;
  * @return C
  */
 function ju_pc() {return ju_new_om(C::class);}
+
+/**
+ * 2020-11-23
+ * @see ju_pc_preserve_absent_f()
+ * "Add an ability to preserve disabled products in a collection
+ * despite of the `cataloginventory/options/show_out_of_stock` option's value": https://github.com/mage2pro/core/issues/148
+ * @used-by \Justuno\M2\Controller\Response\Catalog::execute()
+ * @param C $c
+ * @return C
+ */
+function ju_pc_preserve_absent(C $c) {return $c->setFlag(PAddStock::PRESERVE_ABSENT, true);}
+
+/**
+ * 2020-11-23
+ * 1) "Add an ability to preserve out of stock (including just disabled) products in a collection
+ * despite of the `cataloginventory/options/show_out_of_stock` option's value": https://github.com/mage2pro/core/issues/148
+ * 2) @see ju_pc_preserve_absent() affects only a single explicitly accessible collection.
+ * Sometimes it is not enough:
+ * e.g. when we call @see \Magento\ConfigurableProduct\Model\Product\Type\Configurable::getUsedProducts()
+ * the children collection is created internally (implicitly),
+ * so we can not call @see ju_pc_preserve_absent() for it before it is loaded.
+ * @used-by \Justuno\M2\Catalog\Variants::p()
+ * @param F $f
+ * @return mixed
+ */
+function ju_pc_preserve_absent_f(F $f) {
+	try {
+		$prev = PAddStock::$PRESERVE_ABSENT_F;
+		PAddStock::$PRESERVE_ABSENT_F = true;
+		$r = $f(); /** @var mixed $r */
+	}
+	finally {PAddStock::$PRESERVE_ABSENT_F = $prev;}
+	return $r;
+}
--- lib/Framework/db/table.php
+++ lib/Framework/db/table.php
@@ -3,6 +3,7 @@
  * 2020-08-21 "Port the `df_table_exists` function" https://github.com/justuno-com/core/issues/230
  * @used-by ju_db_from()
  * @used-by ju_table_exists()
+ * @used-by \Justuno\Core\InventoryCatalog\Plugin\Model\ResourceModel\AddStockDataToCollection::aroundExecute()
  * @used-by \Justuno\M2\Setup\UpgradeSchema::tr()
  * @param string|string[] $n
  * @return string
