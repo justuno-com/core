1decd740fb6f6b4053261e327137234288482d35
Dmitry Fedyuk <dfediuk@gmail.com>
2020-11-24 05:33:33 +0600

    1.0.4: https://github.com/mage2pro/core/issues/149

--- lib/Catalog/product/collection.php
+++ lib/Catalog/product/collection.php
@@ -1,5 +1,6 @@
 <?php
 use Closure as F;
+use Justuno\Core\Catalog\Plugin\Model\Indexer\Product\Flat\State as PFlat;
 use Justuno\Core\InventoryCatalog\Plugin\Model\ResourceModel\AddStockDataToCollection as PAddStock;
 use Magento\Catalog\Model\ResourceModel\Product\Collection as C;
 /**
@@ -12,12 +13,38 @@ use Magento\Catalog\Model\ResourceModel\Product\Collection as C;
 function ju_pc() {return ju_new_om(C::class);}
 
 /**
+ * 2020-11-24
+ * @see ju_pc_preserve_absent()
+ * @see ju_pc_preserve_absent_f()
+ * "Add an ability to temporary disable the flat mode for products": https://github.com/mage2pro/core/issues/149
+ * @used-by \Justuno\M2\Controller\Response\Catalog::execute()
+ * @param Closure|null $f [optional]
+ * @return mixed|null
+ */
+function ju_pc_disable_flat(F $f = null) {
+	if (!$f) {
+		PFlat::$DISABLE = true;
+		$r = null;
+	}
+	else {
+		try {
+			$prev = PFlat::$DISABLE;
+			PFlat::$DISABLE = true;
+			$r = $f(); /** @var mixed $r */
+		}
+		finally {PFlat::$DISABLE = $prev;}
+	}
+	return $r;
+}
+
+/**
  * 2020-11-23
  * @see ju_pc_preserve_absent_f()
  * "Add an ability to preserve disabled products in a collection
  * despite of the `cataloginventory/options/show_out_of_stock` option's value": https://github.com/mage2pro/core/issues/148
  * 2020-11-24
  * The solution works only if the «Use Flat Catalog Product» option is disabled.
+ * @see ju_pc_disable_flat()
  * If the the «Use Flat Catalog Product» option is enabled,
  * then the products collection is loaded directly from a `catalog_product_flat_<store>` table,
  * and such tables do not contain disabled products at least in Magento 2.4.0.
@@ -38,6 +65,7 @@ function ju_pc_preserve_absent(C $c) {return $c->setFlag(PAddStock::PRESERVE_ABS
  * so we can not call @see ju_pc_preserve_absent() for it before it is loaded.
  * 2020-11-24
  * The solution works only if the «Use Flat Catalog Product» option is disabled.
+ * @see ju_pc_disable_flat()
  * If the the «Use Flat Catalog Product» option is enabled,
  * then the products collection is loaded directly from a `catalog_product_flat_<store>` table,
  * and such tables do not contain disabled products at least in Magento 2.4.0.
