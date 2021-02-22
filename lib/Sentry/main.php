33370dd8f7bf3980b0d779caa951466718c026bd
Dmitry Fedyuk <dfediuk@gmail.com>
2021-02-22 22:15:17 +0600

    1.0.8: https://github.com/justuno-com/m2/issues/30

--- lib/Sentry/main.php
+++ lib/Sentry/main.php
@@ -13,6 +13,8 @@ use Magento\User\Model\User;
  * 4) `null`: it comes down to the case 1 with the «Justuno_Core» module name.
  * 2020-06-24 "Port the `df_sentry` function": https://github.com/justuno-com/core/issues/118
  * @used-by ju_log()
+ * @used-by \Justuno\M2\Controller\Response\Catalog::execute()
+ * @used-by \Justuno\M2\Response::p()
  * @param string|object|null $m
  * @param _DO|mixed[]|mixed|E $v
  * @param array(string => mixed) $context [optional]
@@ -45,6 +47,21 @@ function ju_sentry($m, $v, array $context = []) {
 }
 
 /**
+ * 2017-01-10
+ * 1) It could be called as `df_sentry_extra(['a' => 'b'])` or df_sentry_extra('a', 'b').
+ * 2) $m could be:
+ * 2.1) A module name: «A_B»
+ * 2.2) A class name: «A\B\C».
+ * 2.3) An object: it comes down to the case 2 via @see get_class()
+ * 2.4) `null`: it comes down to the case 1 with the «Justuno_Core» module name.
+ * 2021-02-22
+ * @used-by \Justuno\M2\Response::store()
+ * @param string|object|null $m
+ * @param mixed ...$v
+ */
+function ju_sentry_extra($m, ...$v) {ju_sentry_m($m)->extra(!$v ? $v : (is_array($v[0]) ? $v[0] : [$v[0] => $v[1]]));}
+
+/**
  * 2016-12-22
  * $m could be:
  * 1) a module name: «A_B»
@@ -53,7 +70,8 @@ function ju_sentry($m, $v, array $context = []) {
  * 4) `null`: it comes down to the case 1 with the «Justuno_Core» module name.
  * 2020-06-26 "Port the `df_sentry_m` function": https://github.com/justuno-com/core/issues/161
  * @used-by ju_sentry()
- * @used-by ju_sentry_m())
+ * @used-by ju_sentry_extra()
+ * @used-by ju_sentry_m()
  * @param string|object|null $m
  * @return Sentry
  */
