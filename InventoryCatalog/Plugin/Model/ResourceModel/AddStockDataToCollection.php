13663e38226d1ee52d372e218fe16256830eb646
Dmitry Fedyuk <dfediuk@gmail.com>
2021-02-26 18:16:01 +0600

    no message

--- InventoryCatalog/Plugin/Model/ResourceModel/AddStockDataToCollection.php
+++ InventoryCatalog/Plugin/Model/ResourceModel/AddStockDataToCollection.php
@@ -67,6 +67,12 @@ final class AddStockDataToCollection {
 
 	/**
 	 * 2020-11-23
+	 * 2021-02-26
+	 * Despite the class uses classes absent in Magento < 2.3:
+	 * @see \Magento\InventoryIndexer\Indexer\IndexStructure and
+	 * @see \Magento\InventoryCatalog\Model\ResourceModel\AddStockDataToCollection
+	 * it still proverly serves the @see PRESERVE_ABSENT constant
+	 * and @see $PRESERVE_ABSENT_F static property (I have checked it).
 	 * @used-by beforeExecute()
 	 * @used-by ju_pc_preserve_absent()
 	 * @used-by \Justuno\Core\CatalogInventory\Plugin\Model\ResourceModel\Stock\Status::beforeAddStockDataToCollection()
@@ -75,6 +81,12 @@ final class AddStockDataToCollection {
 
 	/**
 	 * 2020-11-23
+	 * 2021-02-26
+	 * Despite the class uses classes absent in Magento < 2.3:
+	 * @see \Magento\InventoryIndexer\Indexer\IndexStructure and
+	 * @see \Magento\InventoryCatalog\Model\ResourceModel\AddStockDataToCollection
+	 * it still proverly serves the @see PRESERVE_ABSENT constant
+	 * and @see $PRESERVE_ABSENT_F static property (I have checked it).
 	 * @used-by aroundExecute()
 	 * @used-by ju_pc_preserve_absent_f()
 	 * @used-by \Justuno\Core\CatalogInventory\Plugin\Model\ResourceModel\Stock\Status::beforeAddStockDataToCollection()
