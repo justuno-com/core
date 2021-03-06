07ad09adaa2141b2a075724d8e96a5af8a5c0c21
Dmitry Fedyuk <dfediuk@gmail.com>
2021-03-06 07:22:39 +0600

    https://github.com/justuno-com/core/issues/357

--- lib/Framework/view/page-type.php
+++ lib/Framework/view/page-type.php
@@ -18,3 +18,23 @@ function ju_is_catalog_product_view() {return ju_handle('catalog_product_view');
  * @return bool
  */
 function ju_is_checkout_success() {return ju_handle('checkout_onepage_success');}
+
+/**
+ * 2017-10-15
+ * 2017-12-04
+ * The previous code was:
+ * 		df_handle('adminhtml_system_config_edit')
+ * It is incorrect, because:
+ * 1) It does not take into account the `admin/system_config/save` action.
+ * 2) We do not have any layout handles yet in a @see \Justuno\Core\Config\Backend::dfSaveAfter() handler:
+ * e.g., in the @see \Dfe\Moip\Backend\Enable::dfSaveAfter() handler.
+ * So the following code will not help us:
+ * 		df_handle_prefix('adminhtml_system_config_')
+ * It can be related to the following Moip issue:
+ * "«Please set your Moip private key in the Magento backend» even if the Moip private key is set"
+ * https://github.com/mage2pro/moip/issues/22
+ * 2021-03-06 "Port the `df_is_system_config` function": https://github.com/justuno-com/core/issues/357
+ * @used-by \Justuno\Core\Config\Settings::scope()
+ * @return bool
+ */
+function ju_is_system_config() {return ju_action_prefix('adminhtml_system_config');}
