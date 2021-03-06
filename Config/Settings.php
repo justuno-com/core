1c7ce3b0db2ebcaccdc8d189994a3d4fb0367cea
Dmitry Fedyuk <dfediuk@gmail.com>
2021-03-06 06:35:47 +0600

    https://github.com/justuno-com/core/issues/355

--- Config/Settings.php
+++ Config/Settings.php
@@ -87,9 +87,9 @@ abstract class Settings {
 
 	/**
 	 * 2016-12-24
-	 * Теперь ключи могут начинаться с цифры (например: «3DS»).
-	 * Методы PHP для таких ключей будут содержать приставку «_».
-	 * Например, ключам «test3DS» и «live3DS» соответствует метод
+	 * From now on, keys can have a leading digit (e.g.: «3DS»).
+	 * PHP methods for such keys should be prefixed with «_».
+	 * E.g., the @see \Dfe\Omise\Settings::_3DS() method handles the «test3DS» and «live3DS» keys.
 	 * @used-by v()
 	 * @param string $name
 	 * @return string
