7d6d28006ff0d2d279a25ce1718423ddcf6b5808
Dmitry Fedyuk <dfediuk@gmail.com>
2021-03-07 10:34:23 +0600

    https://github.com/justuno-com/core/issues/363

---
+++ lib/Framework/url/construct.php
@@ -0,0 +1,16 @@
+<?php
+use Magento\Store\Model\Store;
+/**
+ * 2015-11-28
+ * 2016-12-01 If $path is null, '', or '/', then the function will return the frontend root URL.
+ * 2016-12-01 On the frontend side, the @see df_url() behaves identical to df_url_frontend()
+ * 2021-03-07 "Port the `df_url_frontend` function": https://github.com/justuno-com/core/issues/363
+ * @used-by \Justuno\M2\Config\Backend\Debug::dfSaveBefore()
+ * @param string|null $path [optional]
+ * @param array(string => mixed) $p [optional]
+ * @param Store|int|string|null $store [optional]
+ * @return string
+ */
+function ju_url_frontend($path = null, array $p = [], $store = null) {return df_url_trim_index(
+	df_url_frontend_o()->getUrl($path, df_nosid() + $p + (is_null($store) ? [] : ['_store' => ju_store($store)]))
+);}
