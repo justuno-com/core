3c01f20bf47671291d48c63993a30ddde7c68dbd
Dmitry Fedyuk <dfediuk@gmail.com>
2022-11-30 06:49:25 +0000

    refactoring

--- lib/Core/array/find.php
+++ lib/Core/array/find.php
@@ -5,8 +5,8 @@ use Justuno\Core\Exception as DFE;
  * 2020-06-16 "Port the `df_find` function": https://github.com/justuno-com/core/issues/31
  * @used-by ju_ends_with()
  * @used-by ju_starts_with()
- * @param array|callable|\Traversable $a1
- * @param array|callable|\Traversable $a2
+ * @param array|callable|Traversable $a1
+ * @param array|callable|Traversable $a2
  * @param mixed|mixed[] $pAppend [optional]
  * @param mixed|mixed[] $pPrepend [optional]
  * @param int $keyPosition [optional]
@@ -18,7 +18,7 @@ function ju_find($a1, $a2, $pAppend = [], $pPrepend = [], $keyPosition = 0) {
 	# The square bracket syntax for array destructuring assignment (`[…] = […]`) requires PHP ≥ 7.1:
 	# https://github.com/mage2pro/core/issues/96#issuecomment-593392100
 	# We should support PHP 7.0.
-	list($a, $f) = juaf($a1, $a2); /** @var array|\Traversable $a */ /** @var callable $f */
+	list($a, $f) = juaf($a1, $a2); /** @var array|Traversable $a */ /** @var callable $f */
 	$pAppend = ju_array($pAppend); $pPrepend = ju_array($pPrepend);
 	$r = null; /** @var mixed|null $r */
 	foreach ($a as $k => $v) {/** @var int|string $k */ /** @var mixed $v */ /** @var mixed[] $primaryArgument */
