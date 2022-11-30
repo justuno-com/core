ec44e8af41dbd76d9f93184133aff5373acda9c0
Dmitry Fedyuk <dfediuk@gmail.com>
2022-11-30 07:02:50 +0000

    https://github.com/justuno-com/core/issues/374

--- lib/Qa/dump.php
+++ lib/Qa/dump.php
@@ -23,9 +23,8 @@ function ju_dump($v) {return Dumper::i()->dump($v);}
  * @used-by ju_result_s()
  * @used-by juaf()
  * @param mixed $v
- * @return string
  */
-function ju_type($v) {return is_object($v) ? sprintf('an object: %s', get_class($v), ju_dump($v)) : (is_array($v)
+function ju_type($v):string {return is_object($v) ? sprintf('an object: %s', get_class($v), ju_dump($v)) : (is_array($v)
 	? (10 < ($c = count($v)) ? "«an array of $c elements»" : 'an array: ' . ju_dump($v))
 	/** 2020-02-04 We should not use @see df_desc() here */
 	: (is_null($v) ? '`null`' : sprintf('«%s» (%s)', ju_string($v), gettype($v)))
