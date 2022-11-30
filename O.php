f10b7fca5d77a787f4f57c4127e9b9acdb82c3f9
Dmitry Fedyuk <dfediuk@gmail.com>
2022-11-30 08:29:45 +0000

    refactoring

--- O.php
+++ O.php
@@ -73,7 +73,7 @@ class O implements \ArrayAccess {
 	/**
 	 * 2017-07-13
 	 * @used-by self::__construct()
-	 * @used-by a()
+	 * @used-by self::a()
 	 * @var array(string => mixed)
 	 */
 	private $_a;
