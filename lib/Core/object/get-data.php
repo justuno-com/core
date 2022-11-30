209318001519f4a4327925463e674bfeca853ef4
Dmitry Fedyuk <dfediuk@gmail.com>
2022-11-30 08:58:06 +0000

    refactoring

--- lib/Core/object/get-data.php
+++ lib/Core/object/get-data.php
@@ -6,17 +6,6 @@ use Magento\Framework\DataObject as _DO;
 
 /**
  * 2020-02-04
- * 2020-06-18 "Port the `df_gd` function": https://github.com/justuno-com/core/issues/76
- * @param mixed $v
- * @return _DO|AE
- * @throws DFE
- */
-function ju_assert_gd($v) {return ju_has_gd($v) ? $v : ju_error(ju_ucfirst(
-	'%s does not support a proper getData().', ju_type($v)
-));}
-
-/**
- * 2020-02-04
  * 2020-06-18 "Port the `df_has_gd` function": https://github.com/justuno-com/core/issues/77
  * @used-by ju_assert_gd()
  * @used-by \Justuno\Core\Qa\Dumper::dumpObject()
