1b6bc256f7e2cb9dbbc40279523c1e377e253209
Dmitry Fedyuk <dfediuk@gmail.com>
2022-11-30 09:22:48 +0000

    refactoring

--- lib/Core/cache/o.php
+++ lib/Core/cache/o.php
@@ -1,11 +1,5 @@
 <?php
 use Justuno\Core\RAM;
-use Magento\Framework\App\Cache\Frontend\Pool;
-/**
- * 2017-06-30
- * @return Pool
- */
-function ju_cache_pool() {return ju_o(Pool::class);}
 
 /**
  * 2020-06-13 "Port the `df_ram` function": https://github.com/justuno-com/core/issues/8
