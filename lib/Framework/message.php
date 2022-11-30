04400faa07cf197099c8e54cd27cfa84fb69f8e0
Dmitry Fedyuk <dfediuk@gmail.com>
2022-11-30 09:39:38 +0000

    refactoring

--- lib/Framework/message.php
+++ lib/Framework/message.php
@@ -6,7 +6,6 @@ use Magento\Framework\Phrase as P;
 
 /**
  * 2016-08-02 https://mage2.pro/t/974
- * @used-by ju_message_add()
  * @return IMM|MM
  */
 function ju_message_m() {return ju_o(IMM::class);}
