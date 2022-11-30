bb38e59d2ffccf0d45f3f24120daf0d600528c35
Dmitry Fedyuk <dfediuk@gmail.com>
2022-11-30 09:44:57 +0000

    refactoring

--- lib/Framework/module/main.php
+++ lib/Framework/module/main.php
@@ -1,8 +1,5 @@
 <?php
 use Magento\Framework\Module\Manager as MM;
-use Magento\Framework\Module\ModuleList as ML;
-use Magento\Framework\Module\ModuleListInterface as IML;
-
 /**
  * 2019-11-21
  * 2020-08-23 "Port the `df_module_enabled` function" https://github.com/justuno-com/core/issues/282
