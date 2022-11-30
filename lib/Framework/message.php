f71138a028507f09ecdf74e4b86679f7c1d6f633
Dmitry Fedyuk <dfediuk@gmail.com>
2022-11-30 09:39:28 +0000

    refactoring

--- lib/Framework/message.php
+++ lib/Framework/message.php
@@ -5,19 +5,6 @@ use Magento\Framework\Message\MessageInterface as IM;
 use Magento\Framework\Phrase as P;
 
 /**
- * 2016-08-02
- * An arbitrary non-existent identifier allows to preserve the HTML tags in the message.
- * @see \Magento\Framework\View\Element\Message\InterpretationMediator::interpret()
- * https://github.com/magento/magento2/blob/2.1.0/lib/internal/Magento/Framework/View/Element/Message/InterpretationMediator.php#L26-L43
- * 2021-03-06 "Port the `df_message_add` function": https://github.com/justuno-com/core/issues/351
- * @param P|string $s
- * @param string $type
- */
-function ju_message_add($s, $type) {ju_message_m()->addMessage(
-	ju_message_m()->createMessage($type, 'non-existent')->setText(ju_phrase($s)), null
-);}
-
-/**
  * 2016-08-02 https://mage2.pro/t/974
  * @used-by ju_message_add()
  * @return IMM|MM
