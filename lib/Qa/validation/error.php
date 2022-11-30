de33da6cdf4e3338297c3ae4c1f3ed27f6bb55ed
Dmitry Fedyuk <dfediuk@gmail.com>
2022-11-30 08:56:38 +0000

    refactoring

--- lib/Qa/validation/error.php
+++ lib/Qa/validation/error.php
@@ -13,7 +13,6 @@ use Magento\Framework\Phrase;
  * @used-by ju_assert_ne()
  * @used-by ju_assert_nef()
  * @used-by ju_assert_traversable()
- * @used-by ju_call()
  * @used-by ju_caller_m()
  * @used-by ju_customer()
  * @used-by ju_date_from_db()
