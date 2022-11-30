8347d20b1bed3fb07f151a54977093809e12b0c5
Dmitry Fedyuk <dfediuk@gmail.com>
2022-11-30 09:31:24 +0000

    refactoring

--- lib/Core/csv.php
+++ lib/Core/csv.php
@@ -1,15 +1,6 @@
 <?php
 /**
  * 2015-02-07
- * 2021-03-07 "Port the `df_csv_parse` function": https://github.com/justuno-com/core/issues/361
- * @param string|null $s
- * @param string $d [optional]
- * @return string[]
- */
-function ju_csv_parse($s, $d = ',') {return !$s ? [] : ju_trim(explode($d, $s));}
-
-/**
- * 2015-02-07
  * 2020-08-13
  * 1) df_csv(['aaa', 'bbb', 'ccc']) → 'aaa,bbb,ccc'
  * df_csv_pretty(['aaa', 'bbb']) → 'aaa, bbb, ccc'
