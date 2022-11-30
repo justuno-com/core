a44934e3696ba199904d3b584bfb5a372c275852
Dmitry Fedyuk <dfediuk@gmail.com>
2022-11-30 09:52:01 +0000

    refactoring

--- lib/Qa/validation/param.php
+++ lib/Qa/validation/param.php
@@ -3,21 +3,6 @@ use Justuno\Core\Qa\Method as Q;
 use Justuno\Core\Exception as DFE;
 
 /**
- * 2017-04-22
- * 2020-06-20 "Port the `df_param_s` function": https://github.com/justuno-com/core/issues/94
- * @used-by ju_file_write()
- * @used-by ju_report()
- * @param string $v
- * @param int $ord	zero-based
- * @param int $sl [optional]
- * @return string
- * @throws DFE
- */
-function ju_param_s($v, $ord, $sl = 0) {$sl++;
-	return Q::assertValueIsString($v, $sl) ? $v : Q::raiseErrorParam(__FUNCTION__, [Q::S], $ord, $sl);
-}
-
-/**
  * 2020-06-15 "Port the `df_param_sne` function": https://github.com/justuno-com/core/issues/22
  * @used-by ju_date_from_db()
  * @used-by jua_deep()
