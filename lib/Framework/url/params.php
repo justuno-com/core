eedf4d9e4e8d149341cd593e4ee007f06281ad04
Dmitry Fedyuk <dfediuk@gmail.com>
2022-11-30 09:25:31 +0000

    refactoring

--- lib/Framework/url/params.php
+++
@@ -1,6 +0,0 @@
-<?php
-/**
- * 2021-03-07 "Port the `df_nosid` function": https://github.com/justuno-com/core/issues/366
- * @return array(string => bool)
- */
-function ju_nosid() {return ['_nosid' => true];}
