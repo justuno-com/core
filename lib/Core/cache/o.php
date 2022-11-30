7f895f8e84b10e72720d32a2bfa1104a6a2aa842
Dmitry Fedyuk <dfediuk@gmail.com>
2022-11-30 09:22:35 +0000

    refactoring

--- lib/Core/cache/o.php
+++ lib/Core/cache/o.php
@@ -9,7 +9,6 @@ function ju_cache_pool() {return ju_o(Pool::class);}
 
 /**
  * 2020-06-13 "Port the `df_ram` function": https://github.com/justuno-com/core/issues/8
- * @used-by ju_cache_clean()
  * @used-by jucf()
  * @return RAM
  */
