c77a0a02593b2c8b84cebc19517d08bf0c2d5988
Dmitry Fedyuk <dfediuk@gmail.com>
2022-11-30 09:25:11 +0000

    refactoring

--- lib/Framework/url/o.php
+++ lib/Framework/url/o.php
@@ -3,11 +3,6 @@ use Magento\Backend\Model\Url as UrlBackendM;
 use Magento\Backend\Model\UrlInterface as IUrlBackend;
 use Magento\Framework\Url;
 use Magento\Framework\UrlInterface as IUrl;
-/**
- * 2021-03-07 "Port the `df_url_frontend_o` function": https://github.com/justuno-com/core/issues/365
- * @return Url
- */
-function ju_url_frontend_o() {return ju_o(Url::class);}
 
 /**
  * 2020-08-21 "Port the `ju_url_o` function" https://github.com/justuno-com/core/issues/213
