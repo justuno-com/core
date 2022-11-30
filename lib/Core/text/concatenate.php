03340b3d826b93954f866206909fbc703172136b
Dmitry Fedyuk <dfediuk@gmail.com>
2022-11-30 07:09:21 +0000

    refactoring

--- lib/Core/text/concatenate.php
+++ lib/Core/text/concatenate.php
@@ -34,7 +34,6 @@ function ju_cc_n(...$args) {return ju_ccc("\n", jua_flatten($args));}
  * @used-by ju_module_path()
  * @used-by ju_module_path_etc()
  * @used-by ju_url_trim_index()
- * @used-by \Justuno\Core\Config\Backend::value()
  * @param string|string[] ...$args
  * @return string
  */
