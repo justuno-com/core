6e01992aa0bb80576af33564346ba8117cfd3dce
Dmitry Fedyuk <dfediuk@gmail.com>
2022-11-30 08:58:14 +0000

    refactoring

--- lib/Core/object/get-data.php
+++ lib/Core/object/get-data.php
@@ -7,7 +7,6 @@ use Magento\Framework\DataObject as _DO;
 /**
  * 2020-02-04
  * 2020-06-18 "Port the `df_has_gd` function": https://github.com/justuno-com/core/issues/77
- * @used-by ju_assert_gd()
  * @used-by \Justuno\Core\Qa\Dumper::dumpObject()
  * @param mixed $v
  * @return bool
