585711016ba1a9b2a38e1d8081974c8428fb18be
Dmitry Fedyuk <dfediuk@gmail.com>
2022-12-02 07:38:59 +0000

    https://github.com/justuno-com/core/issues/374

--- lib/Checkout/main.php
+++ lib/Checkout/main.php
@@ -23,11 +23,10 @@ function ju_checkout_session() {return ju_o(Session::class);}
  * 2018-10-06
  * 2020-08-24 "Port the `df_order_last` function" https://github.com/justuno-com/core/issues/311
  * @used-by \Justuno\M2\Block\Js::_toHtml()
- * @param bool $required [optional]
  * @return O|null
  * @throws DFE
  */
-function ju_order_last($required = true) {
+function ju_order_last(bool $required = true) {
 	$s = ju_checkout_session(); /** @var Session|DfSession $s */
 	return $s->getLastRealOrderId() ? $s->getLastRealOrder() : (!$required ? null : ju_error());
 }
