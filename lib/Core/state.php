9ab9b4e0c22f78e922f14cfaab2807a63658d200
Dmitry Fedyuk <dfediuk@gmail.com>
2022-12-02 11:22:39 +0000

    https://github.com/justuno-com/core/issues/374

--- lib/Core/state.php
+++ lib/Core/state.php
@@ -6,9 +6,8 @@ use Magento\Framework\App\State;
  * 2015-09-20
  * 2020-06-24 "Port the `df_app_state` function": https://github.com/justuno-com/core/issues/128
  * @used-by ju_area_code()
- * @return State
  */
-function ju_app_state() {return ju_o(State::class);}
+function ju_app_state():State {return ju_o(State::class);}
 
 /**
  * http://mage2.ru/t/37
