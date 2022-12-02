9076bff31f93f0a2f8c3c44ee7f59ff3d4b3c954
Dmitry Fedyuk <dfediuk@gmail.com>
2022-12-02 11:27:39 +0000

    refactoring

--- lib/Core/visitor.php
+++ lib/Core/visitor.php
@@ -6,5 +6,10 @@ use Magento\Framework\HTTP\PhpEnvironment\RemoteAddress as RA;
  * @used-by ju_sentry_m()
  */
 function ju_visitor_ip():string {
-	/** @var RA $a */ $a = ju_o(RA::class); return ju_my_local() ? '92.243.166.8' : $a->getRemoteAddress();
+	/** @var RA $a */ $a = ju_o(RA::class);
+	# 2021-06-11
+	# 1) «Ensure that the Customer IP address is being passed in the API request for all transactions»:
+	# https://github.com/canadasatellite-ca/site/issues/175
+	# 2) https://stackoverflow.com/a/14985633
+	return ju_my_local() ? '158.181.235.66' : jua($_SERVER, 'HTTP_CF_CONNECTING_IP', $a->getRemoteAddress());
 }
