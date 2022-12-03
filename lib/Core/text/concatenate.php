cf98151c25c5ac4d7e62557b593e0ec0d9227369
Dmitry Fedyuk <dfediuk@gmail.com>
2022-12-03 16:54:50 +0000

    https://github.com/justuno-com/core/issues/374

--- lib/Core/text/concatenate.php
+++ lib/Core/text/concatenate.php
@@ -19,10 +19,10 @@ function ju_cc(string $glue, ...$a):string {return implode($glue, jua_flatten($a
  * @used-by \Justuno\Core\Qa\Method::raiseErrorResult()
  * @used-by \Justuno\Core\Qa\Method::raiseErrorVariable()
  * @used-by \Justuno\Core\Qa\Trace\Formatter::frame()
- * @param string|string[] ...$args
+ * @param string|string[] ...$a
  * @return string
  */
-function ju_cc_n(...$args) {return ju_ccc("\n", jua_flatten($args));}
+function ju_cc_n(...$a) {return ju_ccc("\n", jua_flatten($a));}
 
 /**
  * 2020-06-21 "Port the `df_cc_path` function": https://github.com/justuno-com/core/issues/103
