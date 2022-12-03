861669c9b0472fbf023476fb82c999c235a09772
Dmitry Fedyuk <dfediuk@gmail.com>
2022-12-03 16:37:40 +0000

    https://github.com/justuno-com/core/issues/374

--- lib/Core/cache/o.php
+++ lib/Core/cache/o.php
@@ -4,6 +4,5 @@ use Justuno\Core\RAM;
 /**
  * 2020-06-13 "Port the `df_ram` function": https://github.com/justuno-com/core/issues/8
  * @used-by jucf()
- * @return RAM
  */
-function ju_ram() {return RAM::s();}
+function ju_ram():RAM {return RAM::s();}
