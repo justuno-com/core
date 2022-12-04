ef6050eefb46ac7e1fdc99fb1c684f9eb43b1cfe
Dmitry Fedyuk <dfediuk@gmail.com>
2022-12-04 08:27:38 +0000

    https://github.com/justuno-com/core/issues/374

--- lib/Framework/fs/ds.php
+++ lib/Framework/fs/ds.php
@@ -7,7 +7,5 @@ if (!defined('DS')) {
  * 2015-11-30
  * 2020-08-13 "Port the `df_trim_ds_left` function" https://github.com/justuno-com/core/issues/175
  * @used-by ju_path_relative()
- * @param string $p
- * @return string
  */
-function ju_trim_ds_left($p) {return ju_trim_left($p, '/\\');}
+function ju_trim_ds_left(string $p):string {return ju_trim_left($p, '/\\');}
