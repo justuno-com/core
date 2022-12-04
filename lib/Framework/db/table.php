eef1645c33900e4fecec8d86420e060fbe07d91e
Dmitry Fedyuk <dfediuk@gmail.com>
2022-12-04 08:27:23 +0000

    https://github.com/justuno-com/core/issues/374

--- lib/Framework/db/table.php
+++ lib/Framework/db/table.php
@@ -14,7 +14,5 @@ function ju_table($n):string {return jucf(function($n) {return ju_db_resource()-
  * 2019-11-30
  * 2020-08-21 "Port the `df_table_exists` function" https://github.com/justuno-com/core/issues/229
  * @used-by \Justuno\M2\Setup\UpgradeSchema::tr()
- * @param string $t
- * @return bool
  */
-function ju_table_exists($t) {return ju_conn()->isTableExists(ju_table($t));}
+function ju_table_exists(string $t):bool {return ju_conn()->isTableExists(ju_table($t));}
