cbfb55af847b43380ebf33bc3f397120e1ee1e7d
Dmitry Fedyuk <dfediuk@gmail.com>
2022-12-04 08:03:41 +0000

    https://github.com/justuno-com/core/issues/374

--- lib/Framework/registry.php
+++ lib/Framework/registry.php
@@ -13,6 +13,5 @@ function ju_registry(string $k) {return ju_registry_o()->registry($k);}
  * 2015-11-02 https://mage2.pro/t/95
  * 2020-08-24 "Port the `df_registry_o` function" https://github.com/justuno-com/core/issues/309
  * @used-by ju_registry()
- * @return R
  */
-function ju_registry_o() {return ju_o(R::class);}
+function ju_registry_o():R {return ju_o(R::class);}
