ec7764329f6fd0a786e1fef6aaed05aab5fd517f
Dmitry Fedyuk <dfediuk@gmail.com>
2022-12-04 15:40:33 +0000

    https://github.com/justuno-com/core/issues/374

--- lib/Framework/view/handle.php
+++ lib/Framework/view/handle.php
@@ -4,10 +4,8 @@
  * 2020-08-23 "Port the `df_handle` function" https://github.com/justuno-com/core/issues/299
  * @used-by ju_is_catalog_product_view()
  * @used-by ju_is_checkout_success()
- * @param string $n
- * @return bool
  */
-function ju_handle($n) {return in_array($n, ju_handles());}
+function ju_handle(string $n):bool {return in_array($n, ju_handles());}
 
 /**
  * 2015-12-21
