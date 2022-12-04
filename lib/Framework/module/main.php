13150e4ff6cff7e3bc0d21d0939410520e4e9e7d
Dmitry Fedyuk <dfediuk@gmail.com>
2022-12-04 15:16:24 +0000

    https://github.com/justuno-com/core/issues/374

--- lib/Framework/module/main.php
+++ lib/Framework/module/main.php
@@ -11,6 +11,5 @@ function ju_module_enabled(string $m):bool {return ju_module_m()->isEnabled($m);
  * 2019-11-21
  * 2020-08-23 "Port the `ju_module_m` function" https://github.com/justuno-com/core/issues/283
  * @used-by ju_module_enabled()
- * @return MM
  */
-function ju_module_m() {return ju_o(MM::class);}
+function ju_module_m():MM {return ju_o(MM::class);}
