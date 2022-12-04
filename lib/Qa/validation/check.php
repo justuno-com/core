0e313911cd120f0b34f1a6cf0b4c96fb5773cee4
Dmitry Fedyuk <dfediuk@gmail.com>
2022-12-04 22:10:27 +0000

    refactoring

--- lib/Qa/validation/check.php
+++ lib/Qa/validation/check.php
@@ -14,7 +14,6 @@ if (!function_exists('is_iterable')) {
 /**
  * 2020-06-22 "Port the `df_check_s` function": https://github.com/justuno-com/core/issues/109
  * @used-by ju_result_s()
- * @param string $v
- * @return bool
+ * @param mixed $v
  */
-function ju_check_s($v) {return \Justuno\Core\Zf\Validate\StringT::s()->isValid($v);}
+function ju_check_s($v):bool {return \Justuno\Core\Zf\Validate\StringT::s()->isValid($v);}
