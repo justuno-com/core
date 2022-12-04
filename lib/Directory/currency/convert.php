a8e087679a2c9000b9606eacb1cce84a8dc03081
Dmitry Fedyuk <dfediuk@gmail.com>
2022-12-04 07:56:47 +0000

    https://github.com/justuno-com/core/issues/374

--- lib/Directory/currency/convert.php
+++ lib/Directory/currency/convert.php
@@ -9,9 +9,7 @@ use Magento\Store\Model\Store;
  * 2016-09-05
  * 2020-08-26 "Port the `df_currency_convert_from_base` function" https://github.com/justuno-com/core/issues/336
  * @used-by ju_oqi_price()
- * @param float $a
  * @param C|string|null $to
  * @param null|string|int|ScopeA|Store|ConfigData|IConfigData $s [optional]
- * @return float
  */
-function ju_currency_convert_from_base($a, $to, $s = null) {return ju_currency_base($s)->convert($a, $to);}
+function ju_currency_convert_from_base(float $a, $to, $s = null):float {return ju_currency_base($s)->convert($a, $to);}
