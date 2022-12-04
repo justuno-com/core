c0c95b00a8a31895b2167c27d229daa356f89576
Dmitry Fedyuk <dfediuk@gmail.com>
2022-12-04 08:01:49 +0000

    https://github.com/justuno-com/core/issues/374

--- lib/Framework/event.php
+++ lib/Framework/event.php
@@ -7,10 +7,9 @@ use Magento\Framework\Event\ManagerInterface as IManager;
  * https://mage2.pro/t/60
  * 2020-08-24 "Port the `df_dispatch` function" https://github.com/justuno-com/core/issues/316
  * @used-by \Justuno\M2\Controller\Cart\Add::execute()
- * @param string $ev
  * @param array(string => mixed) $d
  */
-function ju_dispatch($ev, array $d = []) {
+function ju_dispatch(string $ev, array $d = []):void {
 	$m = ju_o(IManager::class); /** @var IManager|Manager $m */
 	$m->dispatch($ev, $d);
 }
