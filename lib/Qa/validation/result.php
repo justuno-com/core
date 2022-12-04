bf0eed2baa88bb2d1495f8a9107f8a0f1da7826e
Dmitry Fedyuk <dfediuk@gmail.com>
2022-12-04 22:14:10 +0000

    https://github.com/justuno-com/core/issues/374

--- lib/Qa/validation/result.php
+++ lib/Qa/validation/result.php
@@ -14,12 +14,9 @@ function ju_result_s($v, int $sl = 0):string {return ju_check_s($v) ? $v : Q::ra
 /**
  * 2020-06-22 "Port the `df_result_sne` function": https://github.com/justuno-com/core/issues/107
  * @used-by ju_dts()
- * @param string $v
- * @param int $sl [optional]
- * @return string
  * @throws DFE
  */
-function ju_result_sne($v, $sl = 0) {$sl++;
+function ju_result_sne(string $v, int $sl = 0):string {$sl++;
 	ju_result_s($v, $sl);
 	return '' !== strval($v) ? $v : Q::raiseErrorResult(__FUNCTION__, [Q::NES], $sl);
 }
