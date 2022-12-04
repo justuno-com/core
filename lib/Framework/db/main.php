68fb32f45b69fbe374faf735fa3f22ffc97d7324
Dmitry Fedyuk <dfediuk@gmail.com>
2022-12-04 08:15:26 +0000

    https://github.com/justuno-com/core/issues/374

--- lib/Framework/db/main.php
+++ lib/Framework/db/main.php
@@ -34,6 +34,5 @@ function ju_db_from($t, $cols = '*', $schema = null) {return ju_select()->from(
  * @used-by ju_sentry_m()
  * @see \Magento\Backup\Model\ResourceModel\Helper::getHeader()
  * https://github.com/magento/magento2/blob/2.1.3/app/code/Magento/Backup/Model/ResourceModel/Helper.php#L178
- * @return string
  */
-function ju_db_version() {return jucf(function() {return ju_conn()->fetchRow("SHOW VARIABLES LIKE 'version'")['Value'];});}
+function ju_db_version():string {return jucf(function() {return ju_conn()->fetchRow("SHOW VARIABLES LIKE 'version'")['Value'];});}
