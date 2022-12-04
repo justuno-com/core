6a77477f6f0e05e0fe287683ff045186b8f43f1b
Dmitry Fedyuk <dfediuk@gmail.com>
2022-12-04 08:10:28 +0000

    https://github.com/justuno-com/core/issues/374

--- lib/Framework/db/fetch.php
+++ lib/Framework/db/fetch.php
@@ -5,13 +5,12 @@ use Magento\Framework\DB\Select as S;
  * 2019-11-15
  * 2020-08-22 "Port the `ju_fetch` function" https://github.com/justuno-com/core/issues/266
  * @used-by \Justuno\M2\Config\Source\Brand::map()
- * @param string $t
  * @param string|string[] $cols [optional]
  * @param string|null $compareK [optional]
  * @param int|string|int[]|string[]|null $compareV [optional]
  * @return array(array(string => string))
  */
-function ju_fetch($t, $cols = '*', $compareK = null, $compareV = null) {
+function ju_fetch(string $t, $cols = '*', $compareK = null, $compareV = null) {
 	$s = ju_db_from($t, $cols); /** @var S $s */
 	if (!is_null($compareV)) {
 		$s->where($compareK . ' ' . ju_sql_predicate_simple($compareV), $compareV);
