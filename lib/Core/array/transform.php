ec41157a4f0d0e0b7f6541f22aedc08ea3a3a1f6
Dmitry Fedyuk <dfediuk@gmail.com>
2022-12-04 08:32:14 +0000

    refactoring

--- lib/Core/array/transform.php
+++ lib/Core/array/transform.php
@@ -14,10 +14,14 @@
  * @return array(string => mixed)
  */
 function juak_transform($a1, $a2, bool $req = false):array {
-	# 2020-03-02
-	# The square bracket syntax for array destructuring assignment (`[…] = […]`) requires PHP ≥ 7.1:
+	# 2020-03-02, 2022-10-31
+	# 1) Symmetric array destructuring requires PHP ≥ 7.1:
+	#		[$a, $b] = [1, 2];
 	# https://github.com/mage2pro/core/issues/96#issuecomment-593392100
 	# We should support PHP 7.0.
+	# https://3v4l.org/3O92j
+	# https://www.php.net/manual/migration71.new-features.php#migration71.new-features.symmetric-array-destructuring
+	# https://stackoverflow.com/a/28233499
 	list($a, $f) = juaf($a1, $a2); /** @var array|Traversable $a */ /** @var callable $f */
 	$a = ju_ita($a);
 	$l = array_is_list($a); /** @var bool $l */
