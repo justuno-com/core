4f7ec04644cce77189ab752c673ca030eb3a4095
Dmitry Fedyuk <dfediuk@gmail.com>
2022-12-04 08:03:32 +0000

    https://github.com/justuno-com/core/issues/374

--- lib/Framework/registry.php
+++ lib/Framework/registry.php
@@ -5,10 +5,9 @@ use Magento\Framework\Registry as R;
  * 2015-10-31
  * 2020-08-24 "Port the `df_registry` function" https://github.com/justuno-com/core/issues/308
  * @used-by ju_product_current()
- * @param string $k
  * @return mixed|null
  */
-function ju_registry($k) {return ju_registry_o()->registry($k);}
+function ju_registry(string $k) {return ju_registry_o()->registry($k);}
 
 /**
  * 2015-11-02 https://mage2.pro/t/95
