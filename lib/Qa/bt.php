ce3dc13f5cc3801b6defa6a1eca5d0da7a941ced
Dmitry Fedyuk <dfediuk@gmail.com>
2022-12-04 15:50:47 +0000

    refactoring

--- lib/Qa/bt.php
+++ lib/Qa/bt.php
@@ -5,6 +5,20 @@ use Justuno\Core\Qa\Trace\Formatter;
 
 /**
  * 2021-10-04
+ * @used-by ju_bt_has()
+ * @used-by ju_bt_s()
+ * @used-by ju_caller_entry()
+ * @used-by jus_con()
+ * @used-by \Justuno\Core\Qa\Method::caller()
+ * @param E|int|null|array(array(string => string|int)) $p [optional]
+ * @return array(array(string => mixed))
+ */
+function ju_bt($p = 0, int $limit = 0):array {return is_array($p) ? $p : ($p instanceof E ? $p->getTrace() : ju_slice(
+	debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS, !$limit ? 0 : 1 + $p + $limit), 1 + $p, $limit
+));}
+
+/**
+ * 2021-10-04
  * @used-by ju_bt_log()
  * @used-by ju_bt_s()
  * @used-by ju_caller_entry()
