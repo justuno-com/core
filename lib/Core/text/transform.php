c29d5bf8df729cb8d2985d95edd072313313d294
Dmitry Fedyuk <dfediuk@gmail.com>
2022-12-04 07:38:53 +0000

    https://github.com/justuno-com/core/issues/374

--- lib/Core/text/transform.php
+++ lib/Core/text/transform.php
@@ -4,9 +4,9 @@
  * 2020-08-21 "Port the `df_lcfirst` function" https://github.com/justuno-com/core/issues/219
  * @used-by ju_explode_class_lc()
  * @used-by ju_explode_class_lc_camel()
- * @param string ...$args
+ * @param string|string[] $a
  * @return string|string[]
  */
-function ju_lcfirst(...$args) {return ju_call_a(function($s) {return
+function ju_lcfirst(...$a) {return ju_call_a(function(string $s):string {return
 	mb_strtolower(mb_substr($s, 0, 1)) . mb_substr($s, 1)
-;}, $args);}
+;}, $a);}
