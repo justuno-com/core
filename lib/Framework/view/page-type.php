92da6d8d6855cecf2eae4918bf4e3abb654e37a0
Dmitry Fedyuk <dfediuk@gmail.com>
2022-12-04 15:42:22 +0000

    https://github.com/justuno-com/core/issues/374

--- lib/Framework/view/page-type.php
+++ lib/Framework/view/page-type.php
@@ -3,9 +3,8 @@
  * 2019-03-27
  * 2020-08-23 "Port the `df_is_catalog_product_view` function" https://github.com/justuno-com/core/issues/298
  * @used-by \Justuno\M2\Block\Js::_toHtml()
- * @return bool
  */
-function ju_is_catalog_product_view() {return ju_handle('catalog_product_view');}
+function ju_is_catalog_product_view():bool {return ju_handle('catalog_product_view');}
 
 /**
  * 2017-03-29
