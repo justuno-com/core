4e230f221868ff9be4c8e2444d5dbc9c84f320d0
Dmitry Fedyuk <dfediuk@gmail.com>
2022-12-04 11:18:07 +0300

    no message

---
+++ lib/Framework/db/conn.php
@@ -0,0 +1,16 @@
+<?php
+use Magento\Framework\DB\Adapter\AdapterInterface as IAdapter;
+use Magento\Framework\DB\Adapter\Pdo\Mysql;
+/**
+ * 2020-08-14 "Port the `df_conn` function" https://github.com/justuno-com/core/issues/191
+ * @used-by ju_db_version()
+ * @used-by ju_fetch()
+ * @used-by ju_fetch_col()
+ * @used-by ju_fetch_one()
+ * @used-by ju_select()
+ * @used-by ju_table_exists()
+ * @used-by \Justuno\M2\Store::v()
+ * @used-by \Justuno\M2\Setup\UpgradeSchema::tr()
+ * @return Mysql|IAdapter
+ */
+function ju_conn() {return ju_db_resource()->getConnection();}
--- lib/Framework/db/objects.php
+++ lib/Framework/db/objects.php
@@ -1,24 +1,7 @@
 <?php
 use Magento\Framework\App\ResourceConnection as RC;
-use Magento\Framework\DB\Adapter\AdapterInterface as IAdapter;
-use Magento\Framework\DB\Adapter\Pdo\Mysql;
 use Magento\Framework\DB\Ddl\Trigger;
 use Magento\Framework\DB\Select;
-
-/**
- * 2020-08-14 "Port the `df_conn` function" https://github.com/justuno-com/core/issues/191
- * @used-by ju_db_version()
- * @used-by ju_fetch()
- * @used-by ju_fetch_col()
- * @used-by ju_fetch_one()
- * @used-by ju_select()
- * @used-by ju_table_exists()
- * @used-by \Justuno\M2\Store::v()
- * @used-by \Justuno\M2\Setup\UpgradeSchema::tr()
- * @return Mysql|IAdapter
- */
-function ju_conn() {return ju_db_resource()->getConnection();}
-
 /**
  * 2015-09-29
  * 2020-08-14 "Port the `df_db_resource` function" https://github.com/justuno-com/core/issues/192
