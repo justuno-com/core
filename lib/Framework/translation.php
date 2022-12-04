c096a2d776f1c816db2a5df760989d7a2aa01bb4
Dmitry Fedyuk <dfediuk@gmail.com>
2022-12-04 08:05:14 +0000

    refactoring

--- lib/Framework/translation.php
+++ lib/Framework/translation.php
@@ -17,19 +17,16 @@ function ju_translate_a(array $s, bool $now = false):array {
  * '歐付寶 all/Pay' => 'all-Pay'
  * If you need lower-casing, then use @see df_translit_url_lc() instead.
  * 2020-08-13 "Port the `df_translit_url` function" https://github.com/justuno-com/core/issues/168
- *
  * Example #1: '歐付寶 all/Pay':
- * @see df_fs_name => 歐付寶-allPay
- * @see ju_translit =>  all/Pay
- * @see ju_translit_url => all-Pay
- * @see df_translit_url_lc => all-pay
- *
+ * 		@see df_fs_name => 歐付寶-allPay
+ * 		@see ju_translit =>  all/Pay
+ * 		@see ju_translit_url => all-Pay
+ * 		@see df_translit_url_lc => all-pay
  * Example #2: '歐付寶 O'Pay (allPay)':
- * @see df_fs_name => 歐付寶-allPay
- * @see ju_translit =>  allPay
- * @see ju_translit_url => allPay
- * @see df_translit_url_lc => allpay
- *
+ * 		@see df_fs_name => 歐付寶-allPay
+ * 		@see ju_translit =>  allPay
+ * 		@see ju_translit_url => allPay
+ * 		@see df_translit_url_lc => allpay
  * @used-by \Justuno\Core\Sentry\Client::tags()
  * @param string $s
  * @return string
