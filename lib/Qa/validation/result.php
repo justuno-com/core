ba0db3b8fee558cf3a93014a287337b2c44bf517
Dmitry Fedyuk <dfediuk@gmail.com>
2022-12-04 22:13:32 +0000

    https://github.com/justuno-com/core/issues/374

--- lib/Qa/validation/result.php
+++ lib/Qa/validation/result.php
@@ -4,12 +4,11 @@ use Justuno\Core\Qa\Method as Q;
 /**
  * 2020-06-22 "Port the `df_result_s` function": https://github.com/justuno-com/core/issues/108
  * @used-by ju_result_sne()
- * @param string $v
- * @param int $sl [optional]
+ * @param mixed $v
  * @return string
  * @throws DFE
  */
-function ju_result_s($v, $sl = 0) {return ju_check_s($v) ? $v : Q::raiseErrorResult(
+function ju_result_s($v, int $sl = 0) {return ju_check_s($v) ? $v : Q::raiseErrorResult(
 	__FUNCTION__, [sprintf('A string is required, but got %s.', ju_type($v))], ++$sl
 );}
 
