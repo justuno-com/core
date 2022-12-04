90b979cebd2bd6a11e6d02bb0a4f385105e100c7
Dmitry Fedyuk <dfediuk@gmail.com>
2022-12-04 22:43:16 +0000

    https://github.com/justuno-com/core/issues/374

--- lib/Review/main.php
+++ lib/Review/main.php
@@ -6,9 +6,8 @@ use Magento\Review\Model\Review\Summary as RS;
  * 2020-08-24 "Port the `df_review_summary` function" https://github.com/justuno-com/core/issues/326
  * @used-by \Justuno\M2\Controller\Response\Catalog::execute()
  * @param P|int $p
- * @return RS
  */
-function ju_review_summary($p) {
+function ju_review_summary($p):RS {
 	$r = ju_new_om(RS::class); /** @var RS $r */
 	$r->load(ju_product_id($p));
 	return $r;
