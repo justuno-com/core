c907ac159093708441f49f61d283b4aee87cfc2a
Dmitry Fedyuk <dfediuk@gmail.com>
2022-12-04 07:47:58 +0000

    https://github.com/justuno-com/core/issues/374

--- lib/Cron/main.php
+++ lib/Cron/main.php
@@ -4,6 +4,5 @@
  * ju_cli_argv(1) returns «cron:run» even in the `bin/magento cron:run --bootstrap=standaloneProcessStarted=1` case.
  * 2020-06-17 "Port the `df_is_cron` function": https://github.com/justuno-com/core/issues/45
  * @used-by ju_error()
- * @return bool
  */
-function ju_is_cron() {return ju_is_bin_magento() && 'cron:run' === ju_cli_argv(1);}
+function ju_is_cron():bool {return ju_is_bin_magento() && 'cron:run' === ju_cli_argv(1);}
