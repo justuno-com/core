95fb1ba6ceaac27089ac96957bc93fdb9a086460
Dmitry Fedyuk <dfediuk@gmail.com>
2022-12-04 22:56:58 +0000

    https://github.com/justuno-com/core/issues/374

--- lib/Sales/order/collection.php
+++ lib/Sales/order/collection.php
@@ -4,6 +4,5 @@ use Magento\Sales\Model\ResourceModel\Order\Collection as C;
  * 2019-11-20
  * 2020-08-26 "Port the `df_order_c` function" https://github.com/justuno-com/core/issues/330
  * @used-by \Justuno\M2\Controller\Response\Orders::execute()
- * @return C
  */
-function ju_order_c() {return ju_new_om(C::class);}
+function ju_order_c():C {return ju_new_om(C::class);}
