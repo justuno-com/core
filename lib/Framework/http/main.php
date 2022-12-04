b81720fd33d9b4aa77a43454c930c0bf123d030b
Dmitry Fedyuk <dfediuk@gmail.com>
2022-12-04 15:05:14 +0000

    https://github.com/justuno-com/core/issues/374

--- lib/Framework/http/main.php
+++ lib/Framework/http/main.php
@@ -6,7 +6,7 @@ use Magento\Framework\App\RequestInterface as IRequest;
  * 2020-06-17 "Port the `df_header_utf` function": https://github.com/justuno-com/core/issues/35
  * @used-by ju_error()
  */
-function ju_header_utf() {ju_is_cli() || headers_sent() ?: header('Content-Type: text/html; charset=UTF-8');}
+function ju_header_utf():void {ju_is_cli() || headers_sent() ?: header('Content-Type: text/html; charset=UTF-8');}
 
 /**
  * 2020-06-13 "Port the `df_request` function": https://github.com/justuno-com/core/issues/1
