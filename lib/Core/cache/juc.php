3da9b44140292f48ce8fa470167570e676682616
Dmitry Fedyuk <dfediuk@gmail.com>
2022-12-08 12:55:48 +0000

    refactoring

--- lib/Core/cache/juc.php
+++ lib/Core/cache/juc.php
@@ -12,7 +12,6 @@ use Justuno\Core\RAM;
  * @used-by \Justuno\Core\Format\Html\Tag::tag()
  * @used-by \Justuno\Core\Qa\Failure::report()
  * @used-by \Justuno\Core\Qa\Failure\Exception::e()
- * @used-by \Justuno\Core\Qa\Trace\Frame::context()
  * @used-by \Justuno\Core\Qa\Trace\Frame::functionA()
  * @used-by \Justuno\Core\Qa\Trace\Frame::methodR()
  * @used-by \Justuno\Core\Qa\Trace\Frame::methodParameter()
