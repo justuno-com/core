67bd2b2d5c59323e4706d9e7fff2a469ec46d4c4
Dmitry Fedyuk <dfediuk@gmail.com>
2022-12-13 21:15:29 +0000

    refactoring

--- Zf/Filter/StringTrim.php
+++ Zf/Filter/StringTrim.php
@@ -2,41 +2,50 @@
 namespace Justuno\Core\Zf\Filter;
 # 2020-06-20 "Port the `Justuno\Core\Zf\Filter\StringTrim` class": https://github.com/justuno-com/core/issues/90
 /** @used-by ju_trim() */
-class StringTrim extends \Zend_Filter_StringTrim {
+final class StringTrim extends \Zend_Filter_StringTrim {
 	/**
 	 * 2022-12-14 We can not declare arguments types because they are undeclared in the overriden method.
 	 * @override
-	 * @param string $value
+	 * @see \Zend_Filter_StringTrim::_unicodeTrim()
+	 * @param string $s
 	 * @param string $charlist
-	 * @return string
 	 */
-	protected function _unicodeTrim($value, $charlist = '\\\\s') {
-		if ('' === $value) {
-			$result = $value;
+	protected function _unicodeTrim($s, $charlist = '\\\\s'):string {/** @var string $r */
+		if (ju_es($s)) {
+			$r = $s;
 		}
 		else {
+			# Начало кода из Zend Framework 2.0
 			$chars = preg_replace(
 				['/[\^\-\]\\\]/S', '/\\\{4}/S', '/\//'],
 				['\\\\\\0', '\\', '\/'],
 				$charlist
 			);
   			$pattern = '/^[' . $chars . ']+|[' . $chars . ']+$/usSD';
-			$result = preg_replace($pattern, '', $value);
-			if (null === $result) {
-				$result = $this->_slowUnicodeTrim($value, $charlist);
+			$r = preg_replace($pattern, '', $s);
+			# Конец кода из Zend Framework 2.0
+			if (null === $r) {
+				/**
+				 * Раньше тут происходил вызов @see df_notify_me().
+				 * Заменил вызов @see df_notify_me() на @see df_notify_exception(),
+				 * чтобы установить первоисточник странного поведения системы,
+				 * когда в конец веб-адреса добавляется некий текстовый мусор в неверной кодировке
+				 * (похожий на кусок диагностического сообщения),
+				 * и затем этот веб-адрес попадает в данный метод,
+				 * что приводит нас данную точку.
+				 * Веб-адрес с мусором на конце может быть, например, таким:
+				 * «/contacts/index/++++++++++++++++++++++++++++++++++++++++++Result:+�å+������ü+����û+��ÿ+�������è;»
+				 * Неверность кодировки объясняется, видимо, функциями ядра для работы с веб-адресами.
+				 */
+				$r = $this->_slowUnicodeTrim($s, $charlist);
 			}
 		}
-		return $result;
+		return $r;
 	}
 
-	/**
-	 * @used-by self::_unicodeTrim()
-	 * @param $value
-	 * @param $chars
-	 * @return string
-	 */
-	private function _slowUnicodeTrim($value, $chars) {
-		$utfChars = $this->_splitUtf8($value);
+	/** @used-by self::_unicodeTrim() */
+	private function _slowUnicodeTrim(string $s, string $chars):string {
+		$utfChars = $this->_splitUtf8($s);
 		$pattern = '/^[' . $chars . ']$/usSD';
 		while ($utfChars && preg_match($pattern, $utfChars[0])) {
 			array_shift($utfChars);
