75f7d121f3721462be0ba60fede36b719b02bdd9
Dmitrii Fediuk <dfediuk@gmail.com>
2023-07-29 00:17:46 +0100

    https://github.com/justuno-com/core/issues/393

--- lib/Framework/fs/path.php
+++ lib/Framework/fs/path.php
@@ -2,32 +2,6 @@
 use Magento\Framework\App\Filesystem\DirectoryList as DL;
 
 /**
- * 2020-06-15 "Port the `df_adjust_paths_in_message` function": https://github.com/justuno-com/core/issues/25
- * @used-by ju_xts()
- */
-function ju_adjust_paths_in_message(string $m):string {
-	$bpLen = mb_strlen(BP); /** @var int $bpLen */
-	do {
-		$begin = mb_strpos($m, BP); /** @var int|false $begin */
-		if (false === $begin) {
-			break;
-		}
-		$end = mb_strpos($m, '.php', $begin + $bpLen); /** @var int|false $end */
-		if (false === $end) {
-			break;
-		}
-		$end += 4; # 2016-12-23 It is the length of the «.php» suffix.
-		$m =
-			mb_substr($m, 0, $begin)
-			# 2016-12-23 I use `+ 1` to cut off a slash («/» or «\») after BP.
-			. ju_path_n(mb_substr($m, $begin + $bpLen + 1, $end - $begin - $bpLen - 1))
-			. mb_substr($m, $end)
-		;
-	} while(true);
-	return $m;
-}
-
-/**
  * 2017-05-08
  * 2020-08-13 "Port the `df_path_is_internal` function" https://github.com/justuno-com/core/issues/177
  * @used-by \Justuno\Core\Sentry\Trace::info()
---
+++ lib/Framework/fs/path/sys.php
@@ -0,0 +1,3 @@
+<?php
+use Magento\Framework\Filesystem\Directory\Read as DirectoryRead;
+use Magento\Framework\Filesystem\Directory\ReadInterface as IDirectoryRead;
