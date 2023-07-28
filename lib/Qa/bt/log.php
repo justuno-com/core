0cdd4d4c58001c555bc2f525501261c9171c28d6
Dmitrii Fediuk <dfediuk@gmail.com>
2023-07-28 23:35:14 +0100

    https://github.com/justuno-com/core/issues/393

--- lib/Qa/bt/log.php
+++ lib/Qa/bt/log.php
@@ -1,1 +1,22 @@
 <?php
+use Exception as E;
+use Justuno\Core\Qa\Trace;
+use Justuno\Core\Qa\Trace\Formatter;
+
+/**
+ * 2020-06-16 "Port the `df_bt` function": https://github.com/justuno-com/core/issues/27
+ * @param int|E|array(array(string => string|int)) $p
+ * Позволяет при записи стека вызовов пропустить несколько последних вызовов функций,
+ * которые и так очевидны (например, вызов данной функции, вызов df_bt_log() и т.п.)
+ */
+function ju_bt_log($p = 0):void {ju_report('bt-{date}-{time}.log', ju_bt_s(ju_bt_inc($p)));}
+
+/**
+ * 2020-06-16 "Port the `df_bt_s` function": https://github.com/justuno-com/core/issues/28
+ * @used-by ju_bt_log()
+ * @used-by ju_log_l()
+ * @param int|E|array(array(string => string|int)) $p
+ * Позволяет при записи стека вызовов пропустить несколько последних вызовов функций,
+ * которые и так очевидны (например, вызов данной функции, вызов df_bt_log() и т.п.)
+ */
+function ju_bt_s($p = 0):string {return Formatter::p(new Trace(ju_bt(ju_bt_inc($p))));}
