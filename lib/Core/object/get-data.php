aed4c80d0552daee6799f39ed284d4b459d7e065
Dmitrii Fediuk <dfediuk@gmail.com>
2023-07-29 03:30:50 +0100

    https://github.com/justuno-com/core/issues/393

--- lib/Core/object/get-data.php
+++ lib/Core/object/get-data.php
@@ -18,7 +18,22 @@ function ju_assert_gd($v) {return ju_has_gd($v) ? $v : ju_error(ju_ucfirst(
 
 /**
  * 2020-02-04
+ * @used-by \Justuno\Core\Qa\Dumper::dumpObject()
+ * @param mixed|_DO|AE|oAPI $v
+ * @param F|bool|mixed $onE [optional]
+ * @return array(string => mixed)
+ */
+function ju_gd($v, $onE = true):array {return ju_try(function() use($v) {return
+	# 2023-07-28
+	# "`df_gd()` / `df_has_gd()` / `df_assert_gd` should treat `Magento\Framework\Api\AbstractSimpleObject`
+	# similar to `Magento\Framework\DataObject`": https://github.com/mage2pro/core/issues/290
+	ju_is_api_o(ju_assert_gd($v)) ? $v->__toArray() : $v->getData()
+;}, $onE);}
+
+/**
+ * 2020-02-04
  * 2020-06-18 "Port the `df_has_gd` function": https://github.com/justuno-com/core/issues/77
+ * @used-by ju_assert_gd()
  * @used-by \Justuno\Core\Qa\Dumper::dumpObject()
  * @param mixed $v
  */
