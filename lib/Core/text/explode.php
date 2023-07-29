9f23b911ef0e5369507ce5561e34b82f4ab061f9
Dmitrii Fediuk <dfediuk@gmail.com>
2023-07-29 02:40:42 +0100

    https://github.com/justuno-com/core/issues/393

--- lib/Core/text/explode.php
+++ lib/Core/text/explode.php
@@ -23,7 +23,15 @@ function ju_explode_multiple(array $delimiters, string $s):array {
 function ju_explode_n(string $s):array {return explode("\n", ju_normalize(ju_trim($s)));}
 
 /**
+ * 2016-09-03 Another implementation: df_explode_multiple(['/', DS], $path)
+ * @used-by \Justuno\Core\Qa\Trace\Frame::url()
+ * @return string[]
+ */
+function ju_explode_path(string $p):array {return ju_explode_xpath(ju_path_n($p));}
+
+/**
  * 2020-06-14 "Port the `df_explode_xpath` function": https://github.com/justuno-com/core/issues/20
+ * @used-by ju_explode_path()
  * @used-by jua_deep()
  * @used-by jua_deep_set()
  * @used-by jua_deep_unset()
--- lib/Framework/fs/path/n.php
+++ lib/Framework/fs/path/n.php
@@ -2,6 +2,7 @@
 /**
  * 2020-06-15 "Port the `df_path_n` function": https://github.com/justuno-com/core/issues/26
  * @used-by ju_adjust_paths_in_message()
+ * @used-by ju_explode_path()
  * @used-by ju_file_name()
  * @used-by ju_path_abs()
  * @used-by ju_path_is_internal()
