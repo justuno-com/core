a9fc6347681410e580ec76b25fc6997a8ba1c5c4
Dmitrii Fediuk <dfediuk@gmail.com>
2023-07-29 21:31:34 +0300

    https://github.com/justuno-com/core/issues/397

--- lib/Core/array/merge.php
+++ lib/Core/array/merge.php
@@ -2,6 +2,24 @@
 use Justuno\Core\Exception as DFE;
 
 /**
+ * 2020-06-13 "Port the `dfa_merge_numeric` function": https://github.com/justuno-com/core/issues/14
+ * Plain `array_merge($r, $b)` works wronly,
+ * if $b contains contains SOME numeric-string keys like "99":
+ * https://github.com/mage2pro/core/issues/40#issuecomment-340139933
+ * https://stackoverflow.com/a/5929671
+ * @used-by jua_select_ordered()
+ * @param array(string|int => mixed) $r
+ * @param array(string|int => mixed) $b
+ * @return array(string|int => mixed)
+ */
+function jua_merge_numeric(array $r, array $b):array {
+	foreach ($b as $k => $v) {
+		$r[$k] = $v;
+	}
+	return $r;
+}
+
+/**
  * 2015-02-18
  *  1) По смыслу функция @param array(string => mixed) $defaults
  * @param array(string => mixed) $newValues
@@ -71,24 +89,6 @@ function jua_merge_r(array $defaults, array $newValues):array {/** @var array(st
 				]
 			);
 		}
-	}
-	return $r;
-}
-
-/**
- * 2020-06-13 "Port the `dfa_merge_numeric` function": https://github.com/justuno-com/core/issues/14
- * Plain `array_merge($r, $b)` works wronly,
- * if $b contains contains SOME numeric-string keys like "99":
- * https://github.com/mage2pro/core/issues/40#issuecomment-340139933
- * https://stackoverflow.com/a/5929671
- * @used-by jua_select_ordered()
- * @param array(string|int => mixed) $r
- * @param array(string|int => mixed) $b
- * @return array(string|int => mixed)
- */
-function jua_merge_numeric(array $r, array $b):array {
-	foreach ($b as $k => $v) {
-		$r[$k] = $v;
 	}
 	return $r;
 }
