f8174032e85aea89e4ffd4670081f4c4cf35a4b0
Dmitrii Fediuk <dfediuk@gmail.com>
2023-07-29 05:00:50 +0100

    https://github.com/justuno-com/core/issues/393

--- lib/Framework/module/fs/read.php
+++ lib/Framework/module/fs/read.php
@@ -16,7 +16,13 @@ use Closure as F;
  * @return array(string => mixed)
  */
 function ju_module_file_read($m, string $name, string $ext, F $parser, $onE = true):array {return jucf(
-	function($m, string $name, string $ext, F $parser, $onE = true):array {
+	# 2023-07-26
+	# 1) The previous code was $onE = true:
+	# https://github.com/mage2pro/core/blob/9.9.4/Framework/lib/module/fs/read.php#L57
+	# 2) It led to the error:
+	# `df_sentry_m()` fails: «`Magento_Framework` is not a module, so it does not have subpaths specific for modules»:
+	# https://github.com/mage2pro/core/issues/267
+	function($m, string $name, string $ext, F $parser, $onE):array {
 		$f = ju_module_file_name($m, $name, $ext, $onE);
 		return !$f ? [] : $parser($f);
 	}, func_get_args()
