94dde3c111b7a61de19ac7767cbec57fa7fe5224
Dmitrii Fediuk <dfediuk@gmail.com>
2023-08-30 23:20:53 +0300

    https://github.com/justuno-com/core/issues/403

--- view/base/requirejs-config.js
+++ view/base/requirejs-config.js
@@ -1,2 +1,8 @@
 // 2020-08-23 "Port the `Port the Lodash library" https://github.com/justuno-com/core/issues/273
-var config = {paths: {'ju-lodash': 'https://cdn.jsdelivr.net/lodash/4.14.1/lodash'}};
+var config = {paths: {
+	// 2023-08-30
+	// «Refused to load the script 'https://cdn.jsdelivr.net/lodash/4.14.1/lodash.js'
+	// because it violates the following Content Security Policy directive: "script-src <…>"»:
+	// https://github.com/justuno-com/core/issues/403
+	'ju-lodash': 'https://cdn.jsdelivr.net/lodash/4.14.1/lodash'
+}};
