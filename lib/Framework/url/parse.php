fd3cc337f829dea0f16344e48722fbcd9606b9cd
Dmitrii Fediuk <dfediuk@gmail.com>
2023-08-31 13:22:56 +0100

    https://github.com/justuno-com/core/issues/401

--- lib/Framework/url/parse.php
+++ lib/Framework/url/parse.php
@@ -1,6 +1,6 @@
 <?php
 use Closure as F;
-use Exception as E;
+use Throwable as Th; # 2023-08-30 "Treat `\Throwable` similar to `\Exception`": https://github.com/justuno-com/core/issues/401
 use Zend_Uri as zUri;
 use Zend_Uri_Exception as zUriE;
 use Zend_Uri_Http as zUriH;
@@ -11,7 +11,7 @@ use Zend_Uri_Http as zUriH;
  * @used-by ju_domain_current()
  * @param F|bool|mixed $throw [optional]
  * @return string|null
- * @throws E|zUriE
+ * @throws Th|zUriE
  */
 function ju_domain(string $u, bool $www = false, $throw = true) {return
 	!($r = ju_zuri($u, $throw)->getHost()) ? null : ($www ? $r : ju_trim_text_left($r, 'www.'))
