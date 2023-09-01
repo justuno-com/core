4bd1976e35bab7d71cecc42452aedd359a2f1bac
Dmitrii Fediuk <dfediuk@gmail.com>
2023-09-01 11:49:47 +0100

    https://github.com/justuno-com/core/issues/409

--- lib/Core/text/camel.php
+++ lib/Core/text/camel.php
@@ -3,7 +3,6 @@
  * «YandexMarket» => array(«Yandex», «Market»)
  * «NewNASAModule» => array(«New», «NASA», «Module»)
  * http://stackoverflow.com/a/17122207
- *
  * 2016-08-24
  * https://php.net/manual/reference.pcre.pattern.modifiers.php
  * x (PCRE_EXTENDED)
@@ -18,25 +17,21 @@
  * 		Note, however, that this applies only to data characters.
  * 		Whitespace characters may never appear within special character sequences in a pattern,
  * 		for example within the sequence (?( which introduces a conditional subpattern.
- *
  * 2017-07-09
  * Note 1: ?<=
  * «Zero-width positive lookbehind assertion.
  * Continues match only if the subexpression matches at this position on the left.
  * For example, (?<=19)99 matches instances of 99 that follow 19.
  * This construct does not backtrack.»
- *
  * Note 2: ?=
  * «Zero-width positive lookahead assertion.
  * Continues match only if the subexpression matches at this position on the right.
  * For example, \w+(?=\d) matches a word followed by a digit, without matching the digit.
  * This construct does not backtrack.»
- *
  * I have extracted this explanation from Rad Software Regular Expression Designer
  * (it is a discontinued software, google for it),
  * and it get it from the .NET Framework 3.0 documentation:
  * https://msdn.microsoft.com/en-us/library/bs2twtah(v=vs.85).aspx
- *
  * Note 3.
  * Today I have changed «?=[A-Z0-9]» => «?=[A-Z0-9]», so now it handles the cases with digits, e.g.:
  * «Dynamics365» => [«Dynamics», «365»]
