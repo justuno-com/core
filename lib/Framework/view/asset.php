523f3c2d4b85684947a30df28f12270958456d7b
Dmitrii Fediuk <dfediuk@gmail.com>
2023-09-02 20:06:14 +0100

    https://github.com/justuno-com/core/issues/409

--- lib/Framework/view/asset.php
+++ lib/Framework/view/asset.php
@@ -1,5 +1,7 @@
 <?php
+use Magento\Framework\View\Asset\AssetInterface as IAsset;
 use Magento\Framework\View\Asset\File;
+use Magento\Framework\View\Asset\Remote;
 use Magento\Framework\View\Asset\Repository;
 use Magento\Framework\View\Asset\Source;
 
@@ -13,11 +15,14 @@ function ju_asset():Repository {return ju_o(Repository::class);}
 /**
  * 2015-10-27 http://stackoverflow.com/questions/4659345
  * 2020-08-22 "Port the `df_asset_create` function" https://github.com/justuno-com/core/issues/249
+ * 2023-03-02
+ * «Return value of df_asset_create() must be an instance of Magento\Framework\View\Asset\File,
+ * instance of Magento\Framework\View\Asset\Remote returned»: https://github.com/mage2pro/core/issues/214
  * @used-by ju_asset_exists()
  * @used-by ju_resource_inline()
-
+ * @return IAsset|File|Remote
  */
-function ju_asset_create(string $u):File {$a = ju_asset(); return !ju_is_url_absolute($u)
+function ju_asset_create(string $u):IAsset {$a = ju_asset(); return !ju_is_url_absolute($u)
 	? $a->createAsset($u)
 	: $a->createRemoteAsset($u, jua(['css' => 'text/css', 'js' => 'application/javascript'], ju_file_ext($u)))
 ;}
