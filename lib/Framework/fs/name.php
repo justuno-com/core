a4f334709417f17e234cc8086b8ed8d6b99112c7
Dmitrii Fediuk <dfediuk@gmail.com>
2023-09-02 17:16:05 +0100

    https://github.com/justuno-com/core/issues/409

--- lib/Framework/fs/name.php
+++ lib/Framework/fs/name.php
@@ -22,6 +22,21 @@ function ju_file_name(string $directory, string $template, string $ds = '-'):str
 	$vars = ju_map_k(function($k, $v) use($ds, $now) {return
 		ju_dts($now, implode($ds, $v))
 		;}, ['date' => ['y', 'MM', 'dd'], 'time' => ['HH', 'mm'], 'time-full' => ['HH', 'mm', 'ss']]);
+	/**
+	 * 2016-11-09
+	 * @see \Zend_Date неправильно работает с миллисекундами:
+	 * всегда возвращает 0 вместо реального количества миллисекунд.
+	 * Так происходит из-за дефекта в методах
+	 * @see \Zend_Date::addMilliSecond()
+	 * @see \Zend_Date::setMilliSecond()
+	 * Там такой код:
+	 *		list($milli, $time) = explode(" ", microtime());
+	 *		$milli = intval($milli);
+	 * https://github.com/OpenMage/magento-mirror/blob/1.9.3.0/lib/Zend/Date.php#L4490-L4491
+	 * Этот код ошибочен, потому что после первой операции
+	 * $milli содержит дробное значение меньше 1, например: 0.653...
+	 * А вторая операция тупо делает из этого значения 0.
+	 */
 	$vars['time-full-ms'] = implode($ds, [$vars['time-full'], sprintf(
 		'%02d', round(100 * ju_first(explode(' ', microtime())))
 	)]);
