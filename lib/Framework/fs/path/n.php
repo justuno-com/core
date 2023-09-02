5dd25a9de5d038adc2da34b6e8007356a0a0793a
Dmitrii Fediuk <dfediuk@gmail.com>
2023-09-02 17:04:42 +0100

    https://github.com/justuno-com/core/issues/409

--- lib/Framework/fs/path/n.php
+++ lib/Framework/fs/path/n.php
@@ -1,6 +1,8 @@
 <?php
 /**
+ * Заменяет все сиволы пути на /
  * 2020-06-15 "Port the `df_path_n` function": https://github.com/justuno-com/core/issues/26
+ * 2021-12-17 https://3v4l.org/8iP17
  * @used-by ju_adjust_paths_in_message()
  * @used-by ju_explode_path()
  * @used-by ju_file_name()
@@ -8,4 +10,4 @@
  * @used-by ju_path_is_internal()
  * @used-by ju_path_relative()
  */
-function ju_path_n(string $p):string {return str_replace('//', '/', str_replace('\\', '/', $p));}
+function ju_path_n(string $p):string {return str_replace(['\/', '\\'], '/', $p);}
