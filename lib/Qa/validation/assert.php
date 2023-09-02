762c1919eb0d6c4fa012a81719ab14e1db4205b6
Dmitrii Fediuk <dfediuk@gmail.com>
2023-09-02 21:30:01 +0100

    https://github.com/justuno-com/core/issues/409

--- lib/Qa/validation/assert.php
+++ lib/Qa/validation/assert.php
@@ -45,7 +45,7 @@ function ju_assert($cond, $m = null) {return $cond ?: ju_error($m);}
 function ju_assert_sne(string $v, int $sl = 0):string {
 	$sl++;
 	# The previous code `if (!$v)` was wrong because it rejected the '0' string.
-	return '' !== strval($v) ? $v : Q::raiseErrorVariable(__FUNCTION__, [Q::NES], $sl);
+	return !ju_es($v) ? $v : Q::raiseErrorVariable(__FUNCTION__, [Q::NES], $sl);
 }
 
 /**
